f434c7bf9a39229cecd9c3125240b9b1bbd607bf
Rowan Cockett <rowanc1@gmail.com>
2020-11-18 12:28:18 -0700

    Update numbering, getting ready for figures and code

--- src/state.ts
+++ src/state.ts
@@ -5,6 +5,8 @@ import { escapeHtml } from 'markdown-it/lib/common/utils';
 export enum TargetKind {
   ref='ref',
   figure='fig',
+  table='table',
+  code='code',
   equation='eq',
 }
 
@@ -18,29 +20,38 @@ export type Target = {
 export type StateEnv = {
   targets: Record<string, Target>;
   numbering: {
-    eq: number;
+    equation: number;
+    figure: number;
+    table: number;
+    code: number;
   };
 };
 
 export function getStateEnv(state: {env: any}): StateEnv {
   const env = state.env as StateEnv;
   if (!env.targets) env.targets = {};
+  if (!env.numbering) {
+    env.numbering = {
+      equation: 0,
+      figure: 0,
+      table: 0,
+      code: 0,
+    };
+  }
   if (!state.env) state.env = env;
   return env;
 }
 
 /** Get the next equation number.
  *
- * Can input `{ numbering: { eq:100 } }` to start counting at a different numebr.
+ * Can input `{ numbering: { equation: 100 } }` to start counting at a different numebr.
  *
  * @param state MarkdownIt state that will be modified
  */
 function nextEquationNumber(state: { env: any }) {
-  const env = state.env as StateEnv;
-  if (!env.numbering) env.numbering = { eq: 0 };
-  if (!state.env) state.env = env;
-  env.numbering.eq += 1;
-  return env.numbering.eq;
+  const env = getStateEnv(state);
+  env.numbering.equation += 1;
+  return env.numbering.equation;
 }
 
 /** Create a new internal target.
@@ -65,11 +76,11 @@ export function newTarget(state: { env: any }, id: string) {
  */
 export function newEquation(state: { env: any }, id: string) {
   const env = getStateEnv(state);
-  const num = nextEquationNumber(state);
+  const number = nextEquationNumber(state);
   const target: Target = {
     name: `${TargetKind.equation}-${escapeHtml(id)}`,
     kind: TargetKind.equation,
-    number: num,
+    number,
   };
   env.targets[id] = target;
   return target;
