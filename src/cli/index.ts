ef12e2fe11bde17cff3d3eb294c71efa754d1bf1
Steve Purves <stevejpurves@gmail.com>
2022-01-05 14:50:54 +0000

    added version command

--- src/cli/index.ts
+++ src/cli/index.ts
@@ -1,6 +1,7 @@
 #!/usr/bin/env node
 import commander from 'commander';
 import { addAuthCLI, addExportCLI, addTokenCLI } from './services';
+import version from '../version';
 
 const program = new commander.Command();
 addExportCLI(program);
@@ -8,4 +9,5 @@ addTokenCLI(program);
 addAuthCLI(program);
 
 program.option('-d, --debug [file]', 'Log out any errors to the console or an optional file.');
+program.version(version, '-v, --version', 'output the current version');
 program.parse(process.argv);
