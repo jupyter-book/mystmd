cba837925c1cc8a48ddde9364010949b217d5494
Rowan Cockett <rowanc1@gmail.com>
2022-01-18 20:28:36 -0700

    Improve error message for get latest version

--- src/actions/getLatest.ts
+++ src/actions/getLatest.ts
@@ -9,7 +9,8 @@ export async function getLatestVersion<T extends ALL_BLOCKS>(
 ) {
   const block = await new Block(session, blockId).get();
   const { latest_version } = block.data;
-  if (!latest_version) throw new Error('Block has no versions');
+  if (!latest_version)
+    throw new Error(`Block ${block.data.name} has no versions, do you need to save the draft?`);
   const versionId = { ...block.id, version: latest_version };
   session.log.debug(`Fetching latest version of block: ${versionIdToString(versionId)}`);
   const version = await new Version<T>(session, versionId).get(query);
