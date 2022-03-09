09e57d25745a712afbcb600980fc4dc137eeca45
Rowan Cockett <rowanc1@gmail.com>
2022-03-09 15:59:09 -0700

    🐛 Export singleArticleToPdf

--- src/export/pdf/index.ts
+++ src/export/pdf/index.ts
@@ -1,6 +1,7 @@
 import { exportFromOxaLink } from '../utils';
 import { singleArticleToPdf } from './single';
 
+export { singleArticleToPdf } from './single';
 export { multipleArticleToPdf } from './multiple';
 
 export const oxaLinkToPdf = exportFromOxaLink(singleArticleToPdf);
