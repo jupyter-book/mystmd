a153a86165769fd6a50fa438a5cc4ef4ee392b39
Rowan Cockett <rowanc1@gmail.com>
2022-03-10 12:39:36 -0700

    📦  Unified exports

--- src/index.ts
+++ src/index.ts
@@ -3,3 +3,9 @@ import * as plugins from './plugins';
 export * from './mdast';
 export * from './myst';
 export { plugins };
+
+export { unified } from 'unified';
+export { select, selectAll } from 'unist-util-select';
+export { map } from 'unist-util-map';
+export { visit } from 'unist-util-visit';
+export { remove } from 'unist-util-remove';
