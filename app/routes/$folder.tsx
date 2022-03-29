04a0d5cde3ad364796f4c890e70fcfe5cfd5c419
Rowan Cockett <rowanc1@gmail.com>
2022-03-28 21:47:15 -0600

    🎏  Stretch article at lower breakpoints

--- app/routes/$folder.tsx
+++ app/routes/$folder.tsx
@@ -9,7 +9,7 @@ export const links: LinksFunction = () => {
 export default function Folder() {
   return (
     <article>
-      <main className="mt-[80px] prose prose-stone dark:prose-invert break-words mx-5 p-3 sm:pl-10 md:mr-[250px] lg:pr-[330px] lg:mr-none lg:max-w-[100ch] lg:mx-auto xl:pr-[330px] xl:pl-[390px] xl:max-w-[1475px]">
+      <main className="mt-[80px] prose prose-stone dark:prose-invert break-words mx-5 p-3 max-w-none sm:pl-10 md:mr-[250px] lg:pr-[330px] lg:mr-none lg:mx-auto xl:pr-[330px] xl:pl-[390px] xl:max-w-[1475px]">
         <Outlet />
       </main>
       <TableOfContents />
