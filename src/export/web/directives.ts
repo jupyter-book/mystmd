cb05d76450c6b71d6700d17be5dfad9199011f8f
Rowan Cockett <rowanc1@gmail.com>
2022-04-07 19:24:59 -0600

    🖼  Iframe and margin

--- src/export/web/directives.ts
+++ src/export/web/directives.ts
@@ -1,4 +1,4 @@
-import { Directive, IDirectiveData, directiveOptions, IDirective } from 'mystjs';
+import { Directive, IDirectiveData, directiveOptions, IDirective, Token } from 'mystjs';
 
 const RVar: IDirective = {
   myst: class RVariable extends Directive {
@@ -78,7 +78,86 @@ const Bibliography: IDirective = {
   hast: (h, node) => h(node, 'div', { class: 'bibliography' }),
 };
 
-export const reactiveDirectives = {
+const IFrame: IDirective = {
+  myst: class IFrame extends Directive {
+    public required_arguments = 1;
+
+    public optional_arguments = 0;
+
+    public final_argument_whitespace = false;
+
+    public has_content = false;
+
+    public option_spec = {
+      label: directiveOptions.unchanged,
+      width: directiveOptions.percentage,
+      align: directiveOptions.create_choice(['left', 'center', 'right']),
+    };
+
+    run(data: IDirectiveData<keyof IFrame['option_spec']>) {
+      const token = this.createToken('iframe', 'div', 0, {
+        content: data.body,
+        map: data.bodyMap,
+        block: true,
+        meta: { src: data.args[0] },
+      });
+      return [token];
+    }
+  },
+  mdast: {
+    type: 'iframe',
+    noCloseToken: true,
+    isLeaf: true,
+    getAttrs(t) {
+      return {
+        src: t.meta.src,
+        label: t.meta.label || undefined,
+        width: t.meta.width || undefined,
+        align: t.meta.align || undefined,
+      };
+    },
+  },
+  hast: (h, node) => h(node, 'div', { class: 'iframe' }),
+};
+
+const Margin: IDirective = {
+  myst: class Margin extends Directive {
+    public required_arguments = 0;
+
+    public optional_arguments = 0;
+
+    public final_argument_whitespace = false;
+
+    public has_content = true;
+
+    public option_spec = {};
+
+    run(data: IDirectiveData<keyof Margin['option_spec']>) {
+      const newTokens: Token[] = [];
+      // we create an overall container, then individual containers for the title and body
+      const adToken = this.createToken('margin_open', 'aside', 1, {
+        map: data.map,
+        block: true,
+      });
+      newTokens.push(adToken);
+      const bodyTokens = this.nestedParse(data.body, data.bodyMap[0]);
+      newTokens.push(...bodyTokens);
+      newTokens.push(this.createToken('margin_close', 'aside', -1, { block: true }));
+      return newTokens;
+    }
+  },
+  mdast: {
+    type: 'margin',
+    getAttrs() {
+      return {};
+    },
+  },
+  hast: (h, node) => h(node, 'aside', { class: 'margin' }),
+};
+
+export const directives = {
   'r:var': RVar,
   bibliography: Bibliography,
+  iframe: IFrame,
+  margin: Margin,
 };
