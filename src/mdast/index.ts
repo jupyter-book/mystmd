19d4eb60c4011c6d440bfd7a414a04a95ba87af4
Franklin Koch <franklinwkoch@gmail.com>
2022-04-08 17:38:11 -0600

    Transform for converting html nodes to mdast

--- src/mdast/index.ts
+++ src/mdast/index.ts
@@ -2,6 +2,6 @@ export * from './types';
 export * from './fromMarkdown';
 export { tokensToMyst, Options as MdastOptions } from './tokensToMyst';
 export * from './state';
-export { transform, Options as TransformOptions } from './transforms';
+export * from './transforms';
 export * from './mystToHast';
 export * from './unified';
