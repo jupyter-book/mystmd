0e92814e55bd0c69d2b3e2ee47a2434e7a0c9e80
Rowan Cockett <rowanc1@gmail.com>
2022-04-07 19:24:59 -0600

    🍋  Citations based on label, not identifier

--- src/export/web/transforms/citations.ts
+++ src/export/web/transforms/citations.ts
@@ -14,6 +14,27 @@ function pushCite(references: References, citeRenderer: CitationRenderer, label:
   };
 }
 
+type CiteKind = 'narrative' | 'parenthetical';
+
+function addCitationChildren(
+  cite: GenericNode,
+  renderer: CitationRenderer,
+  kind: CiteKind = 'parenthetical',
+): boolean {
+  const render = renderer[cite.label as string];
+  const children = render?.inline(kind === 'narrative' ? InlineCite.t : InlineCite.p);
+  if (children) {
+    cite.children = children;
+    return true;
+  }
+  cite.error = true;
+  return false;
+}
+
+function hasChildren(node: GenericNode) {
+  return node.children && node.children.length > 0;
+}
+
 export function transformCitations(
   mdast: Root,
   references: References,
@@ -22,24 +43,18 @@ export function transformCitations(
 ) {
   // TODO: this can be simplified if typescript doesn't die on the parent
   selectAll('citeGroup', mdast).forEach((node: GenericNode) => {
-    const { kind } = node;
+    const kind = node.kind as CiteKind;
     node.children?.forEach((cite) => {
-      const citeLabel = cite.identifier as string;
-      cite.children =
-        citeRenderer[citeLabel]?.inline(kind === 'narrative' ? InlineCite.t : InlineCite.p) || [];
+      addCitationChildren(cite, citeRenderer, kind);
     });
   });
   selectAll('cite', mdast).forEach((cite: GenericNode) => {
-    const citeLabel = cite.identifier as string;
+    const citeLabel = cite.label as string;
     // push cites in order of appearance in the document
     pushCite(references, citeRenderer, citeLabel);
-    if (cite.children && cite.children.length > 0) return;
+    if (hasChildren(cite)) return;
     // These are picked up as they are *not* cite groups
-    if (citeRenderer[citeLabel]) {
-      cite.children = citeRenderer[citeLabel]?.inline(InlineCite.t);
-      return;
-    }
-    cache.session.log.error(`⚠️  Could not find citation: ${citeLabel}`);
-    cite.error = true;
+    const success = addCitationChildren(cite, citeRenderer);
+    if (!success) cache.session.log.error(`⚠️  Could not find citation: ${cite.label}`);
   });
 }
