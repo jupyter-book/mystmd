9b8a6d40f1545c54d06dc9e9097495dadb7475af
Rowan Cockett <rowanc1@gmail.com>
2022-04-12 22:22:55 -0600

    ⚠️  Add math errors in rendering

--- app/myst-to-react/math.tsx
+++ app/myst-to-react/math.tsx
@@ -1,5 +1,7 @@
 import { NodeRenderer } from '~/myst-to-react';
 import classNames from 'classnames';
+import { ExclamationIcon } from '@heroicons/react/outline';
+import { InlineMath, Math } from 'myst-spec';
 
 // function Math({ value, html }: { value: string; html: string }) {
 //   const [loaded, setLoaded] = useState(false);
@@ -26,34 +28,58 @@ import classNames from 'classnames';
 //   );
 // }
 
-const mathBlock =
-  (displayMode: boolean): NodeRenderer =>
-  (node) => {
-    if (displayMode) {
+type MathLike = (InlineMath | Math) & {
+  error?: boolean;
+  message?: string;
+  html?: string;
+};
+
+const mathRenderer: NodeRenderer<MathLike> = (node) => {
+  if (node.type === 'math') {
+    if (node.error || !node.html) {
       return (
-        <div
-          className={classNames('relative', { 'mr-[25px]': node.enumerator })}
-          key={node.key}
-        >
-          <div
-            dangerouslySetInnerHTML={{ __html: node.html }}
-            className="overflow-x-auto"
-          />
-          {node.enumerator && (
-            <div className="absolute right-[-25px] m-0 top-[50%] translate-y-[-50%]">
-              ({node.enumerator})
-            </div>
-          )}
-        </div>
+        <pre key={node.key} title={node.message}>
+          <span className="text-red-500">
+            <ExclamationIcon className="inline h-[1em] mr-1" />
+            {node.message}
+            {'\n\n'}
+          </span>
+          {node.value}
+        </pre>
       );
     }
-    return <span key={node.key} dangerouslySetInnerHTML={{ __html: node.html }} />;
-    // return <Math key={node.key} html={node.html} value={node.value as string} />;
-  };
+    return (
+      <div
+        className={classNames('relative', { 'mr-[25px]': node.enumerator })}
+        key={node.key}
+      >
+        <div
+          dangerouslySetInnerHTML={{ __html: node.html }}
+          className="overflow-x-auto"
+        />
+        {node.enumerator && (
+          <div className="absolute right-[-25px] m-0 top-[50%] translate-y-[-50%]">
+            ({node.enumerator})
+          </div>
+        )}
+      </div>
+    );
+  }
+  if (node.error || !node.html) {
+    return (
+      <span key={node.key} className="text-yellow-600" title={node.message}>
+        <ExclamationIcon className="inline h-[1em] mr-1" />
+        {node.value}
+      </span>
+    );
+  }
+  return <span key={node.key} dangerouslySetInnerHTML={{ __html: node.html }} />;
+  // return <Math key={node.key} html={node.html} value={node.value as string} />;
+};
 
 const MATH_RENDERERS = {
-  math: mathBlock(true),
-  inlineMath: mathBlock(false),
+  math: mathRenderer,
+  inlineMath: mathRenderer,
 };
 
 export default MATH_RENDERERS;
