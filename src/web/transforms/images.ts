391b6b2fc07f5c3dc6ac144bc5840348c39cad14
Franklin Koch <franklinwkoch@gmail.com>
2022-04-20 15:20:53 -0600

    Do not try to make image URLs local

--- src/web/transforms/images.ts
+++ src/web/transforms/images.ts
@@ -6,6 +6,13 @@ import { Root } from './types';
 export function transformImages(mdast: Root) {
   const images = selectAll('image', mdast) as GenericNode[];
   images.forEach((image) => {
+    // leave urls alone
+    if (
+      image.url.toLowerCase().startsWith('http:') ||
+      image.url.toLowerCase().startsWith('https:')
+    ) {
+      return;
+    }
     // assumes all images referenced in myst content are moved to public/_static
     image.url = `/_static/${path.basename(image.url)}`;
   });
