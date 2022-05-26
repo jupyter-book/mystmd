d04b5fc3cfc5a360f7385bda2a3259805856d956
Franklin Koch <franklinwkoch@gmail.com>
2022-05-26 15:49:38 -0600

    🐑 Clean up clone: docs, folder -> path, no siteConfig reloading

--- src/sync/clone.ts
+++ src/sync/clone.ts
@@ -5,14 +5,15 @@ import { ISession } from '../session/types';
 import { getDefaultProjectConfig, validateLinkIsAProject } from './utils';
 import { LogLevel } from '../logging';
 import questions from './questions';
-import { loadSiteConfigOrThrow, writeProjectConfig, writeSiteConfig } from '../newconfig';
+import { writeProjectConfig, writeSiteConfig } from '../newconfig';
 import { pullProject } from './pull';
 import { Project } from '../models';
 import { ProjectConfig, SiteConfig, SiteProject } from '../types';
+import { selectors } from '../store';
 
 type Options = {
   remote?: string;
-  folder?: string;
+  path?: string;
   yes?: boolean;
   projectConfig?: ProjectConfig;
 };
@@ -37,44 +38,43 @@ export async function interactiveCloneQuestions(
   projectConfig.remote = project.data.id;
   projectConfig.title = project.data.title;
   projectConfig.description = project.data.description || null;
-  let folder = '.';
-  if (opts?.folder) {
-    if (folder !== '.' && fs.existsSync(folder)) {
-      throw new Error(`Invalid folder for clone: "${folder}", it must not exist.`);
+  let path = '.';
+  if (opts?.path) {
+    if (path !== '.' && fs.existsSync(path)) {
+      throw new Error(`Invalid path for clone: "${path}", it must not exist.`);
     }
   } else {
     const { projectPath } = await inquirer.prompt([
       questions.projectPath(join('content', project.data.name)),
     ]);
-    folder = projectPath;
+    path = projectPath;
   }
   return {
-    siteProject: { path: folder, slug: project.data.name },
+    siteProject: { path, slug: project.data.name },
     projectConfig,
   };
 }
 
 /**
- * Clone a project from curvenote.com to a given path
+ * Interactively clone a project from curvenote.com to a given path
  *
- * Prompts for project link if no project link is given
+ * If a site config is present, the project is also added to the site.
  */
 export async function clone(
   session: ISession,
   remote?: string,
-  folder?: string,
+  path?: string,
   opts?: Pick<Options, 'yes'>,
 ) {
-  let siteConfig: SiteConfig | undefined;
-  try {
-    siteConfig = loadSiteConfigOrThrow(session.store);
-  } catch (error) {
+  // Site config is loaded on session init
+  const siteConfig = selectors.selectLocalSiteConfig(session.store.getState());
+  if (!siteConfig) {
     session.log.debug('Site config not found');
   }
   const { siteProject, projectConfig } = await interactiveCloneQuestions(session, {
     ...opts,
     remote,
-    folder,
+    path,
   });
   writeProjectConfig(session.store, siteProject.path, projectConfig);
   if (siteConfig) {
