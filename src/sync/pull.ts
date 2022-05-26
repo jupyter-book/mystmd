0a589b1fbb9b3341b5c6dddca44d9cb9f35e9930
Franklin Koch <franklinwkoch@gmail.com>
2022-05-26 15:59:25 -0600

    🪢 Clean up pull: docs, allow project pull in pwd, no siteConfig reloading

--- src/sync/pull.ts
+++ src/sync/pull.ts
@@ -9,16 +9,18 @@ import { LogLevel, getLevel } from '../logging';
 import { confirmOrExit } from '../utils';
 import { selectors } from '../store';
 import { isDirectory } from '../toc';
-import { loadProjectConfigOrThrow, loadSiteConfigOrThrow } from '../newconfig';
+import { loadProjectConfigOrThrow } from '../newconfig';
 
 /**
- * Pull content for a project
+ * Pull content for a project on a path
+ *
+ * Errors if project config does not exist or no remote project url is specified.
  */
 export async function pullProject(session: ISession, path: string, opts?: { level?: LogLevel }) {
   const state = session.store.getState();
   const projectConfig = selectors.selectLocalProjectConfig(state, path);
-  if (!projectConfig) throw Error(`cannot pull project from ${path}: no project config`);
-  if (!projectConfig.remote) throw Error(`cannot pull project from ${path}: no remote id`);
+  if (!projectConfig) throw Error(`Cannot pull project from ${path}: no project config`);
+  if (!projectConfig.remote) throw Error(`Cannot pull project from ${path}: no remote project url`);
   const log = getLevel(session.log, opts?.level ?? LogLevel.debug);
   const project = await new Project(session, projectConfig.remote).get();
   const toc = tic();
@@ -33,12 +35,14 @@ export async function pullProject(session: ISession, path: string, opts?: { leve
 }
 
 /**
- * Pull content for all projects in the config.sync that have remotes
+ * Pull content for all projects in the site config
+ *
+ * Errors if no site config is loaded in the state.
  */
 export async function pullProjects(session: ISession, opts: { level?: LogLevel }) {
   const state = session.store.getState();
   const siteConfig = selectors.selectLocalSiteConfig(state);
-  if (!siteConfig) throw Error('cannot pull projects: no site config');
+  if (!siteConfig) throw Error('Cannot pull projects: no site config');
   const limit = pLimit(1);
   await Promise.all(
     siteConfig.projects.map(async (proj) => {
@@ -52,7 +56,14 @@ type Options = {
 };
 
 /**
- * Pull new content for a project from curvenote.com
+ * Pull new project content from curvenote.com
+ *
+ * If this is called from a folder with an existing site configuration and
+ * no other path is specified, all projects included in the site are pulled.
+ * Otherwise, a single project on the specified path is pulled.
+ *
+ * Errors if site config has no projects or if project does not exist
+ * on specified path.
  */
 export async function pull(session: ISession, path?: string, opts?: Options) {
   path = path || '.';
@@ -61,10 +72,11 @@ export async function pull(session: ISession, path?: string, opts?: Options) {
       `Invalid path: "${path}", it must be a folder accessible from the local directory`,
     );
   }
-  if (path === '.') {
-    const siteConfig = loadSiteConfigOrThrow(session.store);
+  // Site config is loaded on session init
+  const siteConfig = selectors.selectLocalSiteConfig(session.store.getState());
+  if (path === '.' && siteConfig) {
     const numProjects = siteConfig.projects.length;
-    if (numProjects === 0) throw new Error('You site configuration has no projects');
+    if (numProjects === 0) throw new Error('Your site configuration has no projects');
     const plural = numProjects > 1 ? 's' : '';
     await confirmOrExit(
       `Pulling will overwrite all content in ${numProjects} project${plural}. Are you sure?`,
@@ -73,7 +85,12 @@ export async function pull(session: ISession, path?: string, opts?: Options) {
     await pullProjects(session, { level: LogLevel.info });
   } else {
     loadProjectConfigOrThrow(session.store, path);
-    await confirmOrExit(`Pulling will overwrite all content in ${path}. Are you sure?`, opts);
+    await confirmOrExit(
+      `Pulling will overwrite all content in ${
+        path === '.' ? 'current directory' : path
+      }. Are you sure?`,
+      opts,
+    );
     await pullProject(session, path, { level: LogLevel.info });
   }
 }
