20cb250637cc1301d6db69668375436f7394d65e
Rowan Cockett <rowanc1@gmail.com>
2022-06-13 15:53:38 -0600

    🐛  KaTeX: Unknown column alignment: *

--- src/transforms/math.ts
+++ src/transforms/math.ts
@@ -1,3 +1,4 @@
+import chalk from 'chalk';
 import katex from 'katex';
 import { Math, InlineMath } from 'myst-spec';
 import { selectAll } from 'mystjs';
@@ -27,13 +28,25 @@ function replaceEqnarray(log: Logger, value: string, file: string) {
     .replace(/\\end{eqnarray}/g, '\\end{align*}');
 }
 
+type RenderResult = { html?: string; warnings?: string[]; error?: string };
+
+function removeWarnings(result: RenderResult, predicate: (warning: string) => boolean) {
+  if (!result.warnings) return result;
+  const nextWarnings = result.warnings.filter(predicate);
+  if (nextWarnings.length === 0) return { html: result.html };
+  return {
+    ...result,
+    warnings: nextWarnings,
+  };
+}
+
 function tryRender(
   log: Logger,
   value: string,
   macros: Record<string, any>,
   displayMode: boolean,
   file: string,
-): { html?: string; warnings?: string[]; error?: string } {
+): RenderResult {
   const warnings: string[] = [];
   try {
     const html = katex.renderToString(value, {
@@ -48,7 +61,7 @@ function tryRender(
   } catch (error) {
     const { message } = error as unknown as Error;
     if (message.includes("Expected 'EOF', got '&' at position")) {
-      log.warn(`Wrapping math with \\begin{align*} in ${file} due to "${message}"`);
+      log.warn(`Math: Wrapping with \\begin{align*} in ${file}\n"${chalk.dim(message)}}"`);
       const result = tryRender(
         log,
         `\\begin{align*}\n${value}\n\\end{align*}`,
@@ -57,6 +70,18 @@ function tryRender(
         file,
       );
       if (result.html) return result;
+    }
+    if (message.includes('Unknown column alignment: *')) {
+      log.warn(`Math: Alignment of "*" not supported, using "c" in ${file}\n${chalk.dim(message)}`);
+      const arrayCentering = /\\begin{array}{((?:\*\{[0-9]+\})c)}/g;
+      if (value.match(arrayCentering)) {
+        const next = value.replace(arrayCentering, '\\begin{array}{c}');
+        const result = tryRender(log, next, macros, displayMode, file);
+        if (result.html) {
+          // We expect, and remove some errors
+          return removeWarnings(result, (w) => !w.includes('Too few columns specified'));
+        }
+      }
     }
     return { error: message };
   }
@@ -79,10 +104,14 @@ export function renderEquation(
     (node as any).html = result.html;
   }
   if (result.warnings) {
-    log.warn(`Math Warning: [${label}]:\n${result.warnings.join('\n')}\n\n${node.value}\n`);
+    log.warn(
+      `Math Warning [${label}] in ${file}:\n${result.warnings.join('\n')}\n\n${node.value}\n`,
+    );
   }
   if (result.error) {
-    log.error(`Math Error: [${label}]: ${result.error}\n\n${node.value}\n`);
+    log.error(
+      `Math Error [${label}] in ${file}:\n${chalk.dim(`${result.error}\n\n${node.value}\n`)}`,
+    );
     (node as any).error = true;
     (node as any).message = result.error;
   }
