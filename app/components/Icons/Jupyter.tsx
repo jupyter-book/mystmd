64d734effa4faf6673cdf54db12f26b32bb29e51
Steve Purves <steve@curvenote.com>
2022-06-23 15:25:59 +0100

    🌘  dark mode tweaks to Jupyter icon #222 (#99)

--- app/components/Icons/Jupyter.tsx
+++ app/components/Icons/Jupyter.tsx
@@ -11,7 +11,10 @@ export const Jupyter = ({
   if (!jupyter) return null;
   return (
     <div
-      className={classnames('not-prose px-1 dark:invert', className)}
+      className={classnames(
+        'not-prose mx-[2px] px-[2px] dark:bg-gray-200 py-[2px] dark:rounded',
+        className,
+      )}
       title="Jupyter Notebook"
     >
       <img src={JupyterLogo} alt="Juyter Logo" width="21" />
