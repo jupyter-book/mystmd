b40f6394d967588e417fa28a1fffc7ad02e1c9be
Rowan Cockett <rowanc1@gmail.com>
2022-06-30 00:03:35 -0600

    🌀 Move components to @curvenote/site

--- app/components/index.tsx
+++
@@ -1,7 +0,0 @@
-export * from './block';
-export * from './ReferencesProvider';
-export * from './theme';
-export * from './DocumentOutline';
-export * from './Navigation';
-export * from './ThemeButton';
-export * from './Frontmatter';
