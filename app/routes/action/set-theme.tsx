516d7dcce7a1ed1e0af58a79bf9893ce3bacbeef
Rowan Cockett <rowanc1@gmail.com>
2022-06-30 00:06:37 -0600

    🧹 linting

--- app/routes/action/set-theme.tsx
+++ app/routes/action/set-theme.tsx
@@ -1,18 +1,18 @@
 import type { ActionFunction } from '@remix-run/node';
 import { json, redirect } from '@remix-run/node';
-import { isTheme, Theme } from '~/components';
+import { isTheme, Theme } from '@curvenote/ui-providers';
 import { getThemeSession } from '~/utils/theme.server';
 
 export const action: ActionFunction = async ({ request }) => {
   const themeSession = await getThemeSession(request);
   const data = await request.json();
   const { theme } = data ?? {};
-  if (!isTheme(theme))
+  if (!isTheme(theme)) {
     return json({
       success: false,
       message: `Invalid theme: "${theme}".`,
     });
-
+  }
   themeSession.setTheme(theme as Theme);
   return json(
     { success: true, theme },
