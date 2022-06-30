96e35f70db1ca4e9dae3a42b5585a43a2b128542
Rowan Cockett <rowanc1@gmail.com>
2022-06-30 00:00:35 -0600

    🪐 Bring back jupyter icons

--- packages/icons/react/jupyter.tsx
+++ packages/icons/react/jupyter.tsx
@@ -1,14 +1,14 @@
 import classnames from 'classnames';
 import { useTheme } from '@curvenote/ui-providers';
-// import JupyterLogo from './jupyter.svg';
-// import JupyterGreyLogo from './jupyter-grey.svg';
+import JupyterLogo from './jupyter.svg';
+import JupyterGreyLogo from './jupyter-grey.svg';
 
 function Jupyter({ className, jupyter }: { className?: string; jupyter: boolean }) {
   const { isDark } = useTheme();
   if (!jupyter) return null;
   return (
     <div className={classnames('not-prose px-1', className)} title="Jupyter Notebook">
-      {/* <img src={isDark ? JupyterGreyLogo : JupyterLogo} alt="Juyter Logo" width="21" /> */}
+      <img src={isDark ? JupyterGreyLogo : JupyterLogo} alt="Juyter Logo" width="21" />
     </div>
   );
 }
