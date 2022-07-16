a66d8e7fb6314cc570e8dde31a28128090a68a5e
Rowan Cockett <rowanc1@gmail.com>
2022-07-16 15:24:40 -0600

    👩‍🔬  FileMock

--- apps/cli/jest.config.cjs
+++ apps/cli/jest.config.cjs
@@ -6,6 +6,8 @@ module.exports = {
     '^.+\\.(ts|tsx)$': 'ts-jest',
   },
   moduleNameMapper: {
+    '\\.(jpg|jpeg|png|gif|eot|otf|webp|svg|ttf|woff|woff2|mp4|webm|wav|mp3|m4a|aac|oga|xml)$':
+      '<rootDir>/tests/__mocks__/fileMock.js',
     mystjs: '<rootDir>/../../node_modules/mystjs/dist/index.umd.js',
   },
   globals: {
---
+++ apps/cli/tests/__mocks__/fileMock.js
@@ -0,0 +1,2 @@
+// https://github.com/facebook/jest/issues/2663#issuecomment-340259853
+module.exports = 'test-file-stub';
