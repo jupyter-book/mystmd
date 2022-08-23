0233df6bc6e43acd3cca5746ba7f8656ae1a7287
Rowan Cockett <rowanc1@gmail.com>
2022-08-23 12:16:24 -0600

    👩‍🔬 Update config files

--- packages/myst-to-tex/jest.config.js
+++ packages/myst-to-tex/jest.config.js
@@ -1,5 +1,10 @@
 module.exports = {
-  preset: 'ts-jest/presets/default-esm', // or other ESM presets
+  preset: 'ts-jest',
+  globals: {
+    'ts-jest': {
+      useESM: true,
+    },
+  },
   moduleNameMapper: {
     mystjs: '<rootDir>/../../node_modules/mystjs/dist/index.umd.js',
   },
