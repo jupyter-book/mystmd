9419d0903ce503593a407989d893a10c46f57e8a
Yuxi Wang <yuxi.wangcd@gmail.com>
2022-08-25 23:08:36 -0600

    Fix jest config transform ignore

--- apps/cli/jest.config.cjs
+++ apps/cli/jest.config.cjs
@@ -1,6 +1,6 @@
 module.exports = {
-  roots: ['<rootDir>'],
-  preset: 'ts-jest/presets/default-esm', // or other ESM presets
+  rootDir: "../../",
+  preset: 'ts-jest/presets/js-with-ts',
   testMatch: ['**/?(*.)+(spec|test).+(ts|tsx|js)'],
   transform: {
     '^.+\\.(ts|tsx)$': 'ts-jest',
@@ -8,15 +8,16 @@ module.exports = {
   testTimeout: 10000,
   moduleNameMapper: {
     '\\.(jpg|jpeg|png|gif|eot|otf|webp|svg|ttf|woff|woff2|mp4|webm|wav|mp3|m4a|aac|oga|xml)$':
-      '<rootDir>/tests/__mocks__/fileMock.js',
-    mystjs: '<rootDir>/../../node_modules/mystjs/dist/index.umd.js',
+      '<rootDir>/apps/cli/tests/__mocks__/fileMock.js',
+    mystjs: '<rootDir>/node_modules/mystjs/dist/index.umd.js',
   },
   globals: {
     'ts-jest': {
-      tsconfig: './tsconfig.json',
+      tsconfig: './tsconfig.test.json',
     },
   },
   verbose: true,
   testEnvironment: 'node',
+  transformIgnorePatterns: ['<rootDir>/node_modules/(?!(vfile|vfile\-message|unified|bail|trough|zwitch|unist\-|hast\-|rehype\-|mdast\-|trim\-|web-namespaces))'],
   testPathIgnorePatterns: ['/node_modules/', '/.yalc/', '/dist/'],
 };
