f72cc4407b07d2792ab378eb7ac3921fde9279b7
Rowan Cockett <rowanc1@gmail.com>
2022-09-07 12:39:27 -0600

    👩‍🔬 Capture myst-to-react changes in tailwind

--- apps/web/tailwind.config.js
+++ apps/web/tailwind.config.js
@@ -5,7 +5,7 @@ module.exports = {
   content: [
     './app/**/*.{js,ts,jsx,tsx}',
     // Look to the actual packages too (better than node_modules for pnpm)
-    '../../packages/myst-util-to-react/src/**/*.{js,ts,jsx,tsx}',
+    '../../packages/myst-to-react/src/**/*.{js,ts,jsx,tsx}',
     '../../packages/site/src/**/*.{js,ts,jsx,tsx}',
     '../../packages/icons/react/**/*.{js,ts,jsx,tsx}',
   ],
