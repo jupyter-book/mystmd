3d6a69d1043fb5d8e91d282c751824f2cd7ec1e9
Rowan Cockett <rowanc1@gmail.com>
2022-09-26 11:16:06 -0600

    🐶 Fetch objects.inv from CDN

--- packages/site/src/loaders/cdn.server.ts
+++ packages/site/src/loaders/cdn.server.ts
@@ -1,5 +1,6 @@
 import type { GenericNode } from 'mystjs';
 import { selectAll } from 'unist-util-select';
+import fetch from 'node-fetch';
 import NodeCache from 'node-cache';
 import { walkPaths } from '@curvenote/nbtx/dist/minify/utils';
 import type {
@@ -20,6 +21,8 @@ declare global {
   // eslint-disable-next-line
   var cdnRouterCache: NodeCache | undefined, configCache: NodeCache | undefined;
 }
+
+const CDN = 'https://cdn.curvenote.com/';
 
 function getCdnRouterCache() {
   if (global.cdnRouterCache) return global.cdnRouterCache;
@@ -52,7 +55,7 @@ async function getCdnPath(hostname: string): Promise<string | undefined> {
 
 function withCDN<T extends string | undefined>(id: string, url: T): T {
   if (!url) return url;
-  return `https://cdn.curvenote.com/${id}/public${url}` as T;
+  return `${CDN}${id}/public${url}` as T;
 }
 
 export async function getConfig(hostname: string): Promise<Config> {
@@ -61,7 +64,7 @@ export async function getConfig(hostname: string): Promise<Config> {
   const cached = getConfigCache().get<Config>(id);
   // Load the data from an in memory cache.
   if (cached) return cached;
-  const response = await fetch(`https://cdn.curvenote.com/${id}/config.json`);
+  const response = await fetch(`${CDN}${id}/config.json`);
   if (response.status === 404) throw responseNoSite();
   const data = (await response.json()) as Config;
   data.id = id;
@@ -83,6 +86,16 @@ export async function getConfig(hostname: string): Promise<Config> {
   return data;
 }
 
+export async function getObjectsInv(hostname: string): Promise<ArrayBuffer | undefined> {
+  const id = await getCdnPath(hostname);
+  if (!id) return;
+  const url = `${CDN}${id}/public/_static/objects.inv`;
+  const response = await fetch(url);
+  if (response.status === 404) return;
+  const buffer = await response.arrayBuffer();
+  return buffer;
+}
+
 export async function getData(
   config?: Config,
   project?: string,
@@ -91,7 +104,7 @@ export async function getData(
   if (!project || !slug || !config) throw responseNoArticle();
   const { id } = config;
   if (!id) throw responseNoSite();
-  const response = await fetch(`https://cdn.curvenote.com/${id}/content/${project}/${slug}.json`);
+  const response = await fetch(`${CDN}${id}/content/${project}/${slug}.json`);
   if (response.status === 404) throw responseNoArticle();
   const data = (await response.json()) as Data;
   if (data?.frontmatter?.thumbnail) {
