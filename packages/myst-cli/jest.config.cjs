176f5e989cfd69f60c1884df4fb5814366cf346f
Rowan Cockett <rowanc1@gmail.com>
2022-10-20 11:49:20 -0600

    👩‍🔬 Jest config magic

--- packages/myst-cli/jest.config.cjs
+++ packages/myst-cli/jest.config.cjs
@@ -1,16 +1,25 @@
 module.exports = {
-  roots: ['<rootDir>'],
-  preset: 'ts-jest/presets/default-esm', // or other ESM presets
-  testMatch: ['**/?(*.)+(spec|test).+(ts|tsx|js)'],
+  rootDir: '../../',
+  preset: 'ts-jest/presets/js-with-ts',
+  testMatch: ['<rootDir>/packages/myst-cli/**/?(*.)+(spec|test).+(ts|tsx|js)'],
   transform: {
     '^.+\\.(ts|tsx)$': 'ts-jest',
   },
+  testTimeout: 10000,
+  moduleNameMapper: {
+    '\\.(jpg|jpeg|png|gif|eot|otf|webp|svg|ttf|woff|woff2|mp4|webm|wav|mp3|m4a|aac|oga|xml)$':
+      '<rootDir>/apps/cli/tests/__mocks__/fileMock.js',
+    '#(.*)': '<rootDir>/node_modules/$1', // https://github.com/chalk/chalk/issues/532
+  },
   globals: {
     'ts-jest': {
-      tsconfig: './tsconfig.json',
+      tsconfig: './tsconfig.test.json',
     },
   },
   verbose: true,
   testEnvironment: 'node',
-  testPathIgnorePatterns: ['/node_modules/', '/dist/'],
+  transformIgnorePatterns: [
+    '<rootDir>/node_modules/(?!(vfile|formdata-polyfill|chalk|fetch-blob|vfile-message|unified|bail|trough|zwitch|unist-|hast-|rehype-|mdast-|trim-|web-namespaces))',
+  ],
+  testPathIgnorePatterns: ['/node_modules/', '/.yalc/', '/dist/'],
 };
