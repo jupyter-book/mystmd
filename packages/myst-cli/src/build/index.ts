c22c1dbe6856647754f73fd5bc6e5d78bb2c4dca
Franklin Koch <franklinwkoch@gmail.com>
2022-10-28 00:04:08 -0600

    🏗 Move around myst init function

--- packages/myst-cli/src/build/index.ts
+++ packages/myst-cli/src/build/index.ts
@@ -1,5 +1,7 @@
 export * from './build';
+export * from './clean';
 export * from './docx';
+export * from './init';
 export * from './pdf';
 export * from './tex';
 export * from './types';
--- packages/myst-cli/src/cli/init.ts
+++ packages/myst-cli/src/cli/init.ts
@@ -1,52 +1,7 @@
-import fs from 'fs';
-import path from 'path';
 import { Command, Option } from 'commander';
-import { loadConfigAndValidateOrThrow } from '../config';
-import { loadProjectFromDisk } from '../project';
-import { selectors } from '../store';
-import type { ISession } from '../session';
+import { init } from '../build';
 import { Session } from '../session';
 import { clirun } from './clirun';
-
-const PROJECT_CONFIG = `version: 1
-project:
-  # title: 
-  # description:
-  # venue: 
-  # github: 
-  # arxiv:
-  # open_access:
-  # license: 
-  # doi:
-  # date:
-  # index:
-  # subject:
-  keywords: []
-  authors: []
-  bibliography: []
-  exclude: []
-`;
-
-export type InitOptions = {
-  writeToc: boolean;
-};
-
-export function init(session: ISession, opts: InitOptions) {
-  const { writeToc } = opts;
-  loadConfigAndValidateOrThrow(session, '.');
-  const projectConfig = selectors.selectLocalProjectConfig(session.store.getState(), '.');
-  if (projectConfig) {
-    const projectConfigFile = selectors.selectLocalConfigFile(session.store.getState(), '.');
-    session.log.info(`✋ Project already initialized with config file: ${projectConfigFile}`);
-  } else {
-    const [configFile] = session.configFiles;
-    session.log.info(`💾 Writing project config file: ${path.resolve(configFile)}`);
-    fs.writeFileSync(configFile, PROJECT_CONFIG);
-  }
-  if (writeToc) {
-    loadProjectFromDisk(session, '.', { writeToc });
-  }
-}
 
 export function makeWriteTocOption() {
   return new Option(
