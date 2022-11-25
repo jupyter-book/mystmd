97a888c021ce0464efa2d696e93d3f6d81c45ef3
Rowan Cockett <rowanc1@gmail.com>
2022-11-25 11:18:04 -0700

    🤫 Allow toText to take a null (#91)

---
+++ .changeset/khaki-ducks-turn.md
@@ -0,0 +1,5 @@
+---
+'myst-common': patch
+---
+
+Allow toText to take a null
--- packages/myst-common/src/utils.ts
+++ packages/myst-common/src/utils.ts
@@ -88,7 +88,7 @@ export function setTextAsChild(node: Partial<Parent>, text: string) {
   node.children = [{ type: 'text', value: text } as Node];
 }
 
-export function toText(content?: Node[] | Node): string {
+export function toText(content?: Node[] | Node | null): string {
   if (!content) return '';
   if (!Array.isArray(content)) return toText([content]);
   return (content as PhrasingContent[])
