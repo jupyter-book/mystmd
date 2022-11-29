d72da3508de1e2e58ff584f199a4ff6588c92c3b
Franklin Koch <franklinwkoch@gmail.com>
2022-11-29 10:42:33 -0700

    🧪🧹 Tidy tests

--- packages/myst-config/src/site/site.spec.ts
+++ packages/myst-config/src/site/site.spec.ts
@@ -103,17 +103,7 @@ describe('validateSiteConfig', () => {
       nav: [{ title: 'cool folder', children: [{ title: 'cool page', url: '/test/cool-page' }] }],
       actions: [{ title: 'Go To Example', url: 'https://example.com', static: false }],
       domains: ['test.curve.space'],
-      twitter: 'test',
-      logo: 'curvenote.png',
-      logo_text: 'test logo',
       favicon: 'curvenote.png',
-      analytics: {
-        google: 'google',
-        plausible: 'plausible',
-      },
-      design: {
-        hide_authors: true,
-      },
     };
     expect(validateSiteConfig(siteConfig, opts)).toEqual(siteConfig);
   });
