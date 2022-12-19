8124f00fb877b197591b97fafe8beb19e1e6c6b0
Franklin Koch <franklinwkoch@gmail.com>
2022-12-19 12:05:57 -0700

    🧪 Fix up jtex test config

--- packages/jtex/jest.config.cjs
+++ packages/jtex/jest.config.cjs
@@ -1,6 +1,6 @@
 module.exports = {
   roots: ['<rootDir>'],
-  preset: 'ts-jest/presets/default-esm', // or other ESM presets
+  preset: 'ts-jest/presets/js-with-ts', // or other ESM presets
   testMatch: ['**/?(*.)+(spec|test).+(ts|tsx|js)'],
   testTimeout: 20000,
   transform: {
@@ -8,10 +8,14 @@ module.exports = {
   },
   globals: {
     'ts-jest': {
-      tsconfig: './tsconfig.base.json',
+      tsconfig: './tsconfig.test.json',
     },
   },
   verbose: true,
   testEnvironment: 'node',
+  transformIgnorePatterns: [
+    '<rootDir>/packages/jtex/node_modules/(?!(vfile|formdata-polyfill|chalk|fetch-blob|vfile-message|unified|bail|trough|zwitch|unist-|hast-|html-|rehype-|mdast-|micromark-|trim-|web-namespaces|property-information|space-separated-tokens|comma-separated-tokens|get-port|stringify-entities|character-entities-html4|ccount))',
+    '<rootDir>/node_modules/(?!(vfile|formdata-polyfill|chalk|fetch-blob|vfile-message|unified|bail|trough|zwitch|unist-|hast-|html-|rehype-|mdast-|micromark-|trim-|web-namespaces|property-information|space-separated-tokens|comma-separated-tokens|get-port|stringify-entities|character-entities-html4|ccount))',
+  ],
   testPathIgnorePatterns: ['/node_modules/', '/dist/'],
 };
