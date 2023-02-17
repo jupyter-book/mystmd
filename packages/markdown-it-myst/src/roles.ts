b4776a3d0782be47ca83c331e81257969c963112
Franklin Koch <franklinwkoch@gmail.com>
2023-02-17 12:55:21 -0700

    ◻️ Change safari role regex as well

--- packages/markdown-it-myst/src/roles.ts
+++ packages/markdown-it-myst/src/roles.ts
@@ -50,7 +50,7 @@ try {
 } catch (error) {
   // Safari does not support negative look-behinds
   // This is a slightly down-graded variant, as it does not require a space.
-  _x = /^\{([a-zA-Z_\-+:]{1,36})\}(`+)(?!`)(.+?)\2(?!`)/;
+  _x = /^\{\s*([a-zA-Z_\-+:]{1,36})\s*\}(`+)(?!`)(.+?)\2(?!`)/;
 }
 const ROLE_PATTERN = _x;
 
