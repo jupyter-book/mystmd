78bb237ead2e2de6672d47d2140b3b8dbbad08fc
Rowan Cockett <rowanc1@gmail.com>
2023-02-17 00:10:12 -0700

    🧬 Support github-style admonitions

---
+++ .changeset/bright-buckets-kneel.md
@@ -0,0 +1,6 @@
+---
+'myst-transforms': patch
+'myst-cli': patch
+---
+
+Support github-style admonitions, add the `simple` class
--- packages/myst-transforms/src/admonitions.ts
+++ packages/myst-transforms/src/admonitions.ts
@@ -1,13 +1,16 @@
 import type { Plugin } from 'unified';
 import type { Root } from 'mdast';
-import type { Admonition, AdmonitionTitle, FlowContent } from 'myst-spec';
+import type { Admonition, AdmonitionTitle, Blockquote, FlowContent } from 'myst-spec';
 import { selectAll } from 'unist-util-select';
 import { AdmonitionKind } from './types';
+import type { GenericNode } from 'myst-common';
 
 type Options = {
   /** Replace the admonition title with the first paragraph if it is all bold. */
   replaceAdmonitionTitles?: boolean;
 };
+
+const githubAdmonitionKinds = ['note', 'warning'];
 
 export function admonitionKindToTitle(kind: AdmonitionKind | string) {
   const transform: Record<string, string> = {
@@ -67,6 +70,41 @@ export function admonitionHeadersTransform(tree: Root, opts?: Options) {
   });
 }
 
+/**
+ * Visit all blockquote notes and add headers if necessary, support GitHub style admonitions
+ */
+export function admonitionBlockquoteTransform(tree: Root) {
+  const blockquote = selectAll('blockquote', tree) as Blockquote[];
+  blockquote.forEach((node: GenericNode) => {
+    if (
+      !node.children ||
+      node.children?.[0]?.type !== 'paragraph' ||
+      node.children[0].children?.[0]?.type !== 'strong'
+    ) {
+      return;
+    }
+    const strong = node.children[0].children[0];
+    if (strong.children?.[0].type !== 'text') return;
+    const kind = strong.children[0].value?.trim().toLowerCase() ?? '';
+    if (!githubAdmonitionKinds.includes(kind)) return;
+    node.type = 'admonition';
+    node.kind = kind;
+    node.class = node.class ? node.class + ' simple' : 'simple';
+    node.children[0].children.splice(0, 1); // Get rid of the strong node
+    node.children = [
+      {
+        type: 'admonitionTitle',
+        children: [{ type: 'text', value: admonitionKindToTitle(node.kind) }],
+      },
+      ...node.children,
+    ];
+  });
+}
+
 export const admonitionHeadersPlugin: Plugin<[Options?], Root, Root> = (opts) => (tree) => {
   admonitionHeadersTransform(tree, opts);
+};
+
+export const admonitionBlockquotePlugin: Plugin<[], Root, Root> = () => (tree) => {
+  admonitionBlockquoteTransform(tree);
 };
