f53a5ebadee4d83c231dfa08ed060b762285f0cd
Franklin Koch <franklinwkoch@gmail.com>
2023-02-22 23:45:03 -0700

    💬 Support myst comment node in docx/html/jats

--- packages/myst-to-jats/src/index.ts
+++ packages/myst-to-jats/src/index.ts
@@ -115,6 +115,9 @@ const handlers: Record<string, Handler> = {
   mystComment() {
     // Do not archive comments
   },
+  comment() {
+    // Do not archive comments
+  },
   strong(node, state) {
     // TODO: potentially add `{ role: 'strong' }`?
     state.renderInline(node, 'bold');
