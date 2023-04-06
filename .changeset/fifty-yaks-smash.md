ea0c73b5bde29052fa17a7c50d5b44e27316cd75
Franklin Koch <franklinwkoch@gmail.com>
2023-04-06 17:43:24 -0600

    🧪 Add placeholder for end-to-end cli tests

---
+++ .changeset/fifty-yaks-smash.md
@@ -0,0 +1,5 @@
+---
+'myst-cli': patch
+---
+
+Add placeholder for end-to-end cli tests
---
+++ packages/myst-cli/tests/endToEnd.spec.ts
@@ -0,0 +1,54 @@
+import fs from 'fs';
+import path from 'path';
+import yaml from 'js-yaml';
+import { exec } from 'myst-cli-utils';
+
+type TestFile = {
+  cases: TestCase[];
+};
+type TestCase = {
+  title: string;
+  cwd: string;
+  command: string;
+  outputs: {
+    path: string;
+    content: string;
+  }[];
+};
+
+const directory = path.join('tests');
+
+function loadCases(file: string) {
+  const testYaml = fs.readFileSync(path.join(directory, file)).toString();
+  return (yaml.load(testYaml) as TestFile).cases;
+}
+
+function resolve(relative: string) {
+  return path.resolve(__dirname, relative);
+}
+
+describe('End-to-end cli export tests', () => {
+  const cases = loadCases('exports.yml');
+  test.each(cases.map((c): [string, TestCase] => [c.title, c]))(
+    '%s',
+    async (_, { cwd, command, outputs }) => {
+      // Clean expected outputs if they already exist
+      await Promise.all(
+        outputs.map(async (output) => {
+          if (fs.existsSync(resolve(output.path))) {
+            await exec(`rm ${resolve(output.path)}`, { cwd: resolve(cwd) });
+          }
+        }),
+      );
+      // Run CLI command
+      await exec(command, { cwd: resolve(cwd) });
+      // Expect correct output
+      outputs.forEach((output) => {
+        expect(fs.existsSync(resolve(output.path))).toBeTruthy();
+        expect(fs.readFileSync(resolve(output.path), { encoding: 'utf-8' })).toEqual(
+          fs.readFileSync(resolve(output.content), { encoding: 'utf-8' }),
+        );
+      });
+    },
+  );
+});
