dbf19bcf370215e5901526cd7305db72d9793c91
Franklin Koch <franklinwkoch@gmail.com>
2023-04-20 18:33:38 -0600

    🔧 Fix single file export references by building project files

--- packages/myst-directives/src/embed.ts
+++ packages/myst-directives/src/embed.ts
@@ -16,12 +16,11 @@ export const embedDirective: DirectiveSpec = {
     },
   },
   run(data: DirectiveData): GenericNode[] {
-    const { label, identifier } = normalizeLabel(data.options?.label as string) || {};
+    const { label } = normalizeLabel(data.options?.label as string) || {};
     return [
       {
         type: 'embed',
         label,
-        identifier,
         'remove-input': data.options?.['remove-input'],
         'remove-output': data.options?.['remove-output'],
       },
