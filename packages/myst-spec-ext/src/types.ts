f7a6252df1e4262bdc309fc1114a06ce0ce090ac
Ubuntu <ubuntu@ip-172-31-11-234.ec2.internal>
2023-06-05 19:10:22 +0000

    specialize visibility type

--- packages/myst-spec-ext/src/types.ts
+++ packages/myst-spec-ext/src/types.ts
@@ -70,7 +70,7 @@ export type Admonition = SpecAdmonition & {
 
 export type Code = SpecCode & {
   executable?: boolean;
-  visibility?: string;
+  visibility?: 'show' | 'hide' | 'remove';
 };
 
 export type ListItem = SpecListItem & {
