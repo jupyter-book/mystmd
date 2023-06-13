b015d407c7295c63dd4fcacacdca2174fada32b4
Franklin Koch <franklinwkoch@gmail.com>
2023-06-13 17:45:21 -0600

    🔧 Make the article suffix more unique

--- packages/myst-to-jats/src/utils.ts
+++ packages/myst-to-jats/src/utils.ts
@@ -1,7 +1,7 @@
 import type { IJatsSerializer } from './types.js';
 
 export function notebookArticleSuffix(state: IJatsSerializer) {
-  return state.data.isNotebookArticleRep ? '-article' : '';
+  return state.data.isNotebookArticleRep ? '-jats-article' : '';
 }
 
 export function slugSuffix(state: IJatsSerializer) {
