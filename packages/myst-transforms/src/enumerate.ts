751eeabbb4f40bd62bcef5e05029e2fed2e53c0c
Franklin Koch <franklinwkoch@gmail.com>
2023-06-19 17:22:26 -0600

    #️⃣ Add hash to cross-references when we know it's missing

--- packages/myst-transforms/src/enumerate.ts
+++ packages/myst-transforms/src/enumerate.ts
@@ -552,6 +552,7 @@ export const resolveReferenceLinksTransform = (tree: Root, opts: StateOptions) =
           source: TRANSFORM_NAME,
         },
       );
+      link.url = `#${link.url}`;
     }
     // Change the link into a cross-reference!
     const xref = link as unknown as CrossReference;
