c5a08a9af16b36fd10a48085a6b9451e0efe8a79
Franklin Koch <franklinwkoch@gmail.com>
2023-07-04 13:31:22 -0600

    📑 Copy dependent images, included files, etc into meca bundle

--- packages/myst-cli/src/build/meca/index.ts
+++ packages/myst-cli/src/build/meca/index.ts
@@ -1,16 +1,22 @@
 import fs from 'node:fs';
 import path from 'node:path';
+import AdmZip from 'adm-zip';
 import { glob } from 'glob';
+import mime from 'mime-types';
 import { tic } from 'myst-cli-utils';
 import { ExportFormats } from 'myst-frontmatter';
 import type { LinkTransformer } from 'myst-transforms';
+import { selectAll } from 'unist-util-select';
 import { js2xml } from 'xml-js';
 import { findCurrentProjectAndLoad } from '../../config.js';
+import { loadFile } from '../../process/index.js';
 import type { LocalProjectPage } from '../../project/index.js';
 import { loadProjectFromDisk, writeTocFromProject } from '../../project/index.js';
+import { castSession } from '../../session/index.js';
 import type { ISession } from '../../session/types.js';
 import { selectors } from '../../store/index.js';
-import { KNOWN_IMAGE_EXTENSIONS, createTempFolder, isDirectory } from '../../utils/index.js';
+import { createTempFolder, isDirectory } from '../../utils/index.js';
+import { runJatsExport } from '../jats/single.js';
 import type { ExportWithOutput, ExportOptions } from '../types.js';
 import {
   cleanOutput,
@@ -18,8 +24,6 @@ import {
   collectExportOptions,
   resolveAndLogErrors,
 } from '../utils/index.js';
-import { runJatsExport } from '../jats/single.js';
-import AdmZip from 'adm-zip';
 
 type ManifestItem = {
   href: string;
@@ -27,6 +31,11 @@ type ManifestItem = {
   mediaType: string;
 };
 
+/**
+ * Copy "file" maintaining original relative path to "from" directory with final "to" directory
+ *
+ * If "file" is not inside "from" folder, the file is not copied.
+ */
 function copyFileMaintainPath(session: ISession, file: string, from: string, to: string) {
   // File must be inside "from" folder
   if (!path.resolve(path.relative(from, file)).startsWith(path.resolve(from))) {
@@ -46,6 +55,11 @@ function copyFileMaintainPath(session: ISession, file: string, from: string, to:
   }
 }
 
+/**
+ * Copy "file" to "to" directory.
+ *
+ * If a file already exists with the basename of "file" inside "to" directory, it is not copied.
+ */
 function copyFileToFolder(session: ISession, file: string, to: string) {
   const destination = path.join(to, path.basename(file));
   if (fs.existsSync(destination)) {
@@ -64,21 +78,12 @@ function copyFileToFolder(session: ISession, file: string, to: string) {
 }
 
 function mediaTypeFromFile(file: string) {
-  // I'm sure there's an actual library to do this.
+  const mediaType = mime.lookup(file);
+  if (mediaType) return mediaType;
   const ext = path.extname(file);
-  if (!ext) return 'application/octet-stream';
-  if (ext === '.docx') {
-    return 'application/vnd.openxmlformats-officedocument.wordprocessingml.document';
-  }
-  if (ext === '.pdf') return 'application/pdf';
-  if (ext === '.md') return 'text/markdown';
-  if (['.html', '.txt'].includes(ext)) return `text/${ext.substring(1)}`;
-  if ((KNOWN_IMAGE_EXTENSIONS as string[]).includes(ext)) return `image/${ext.substring(1)}`;
-  if (ext === '.tex') return 'application/x-latex';
-  if (ext === 'ipynb') return 'application/x-ipynb+json';
+  // A couple common extras not included with 'mime-types'
+  if (ext === '.ipynb') return 'application/x-ipynb+json';
   if (ext === '.bib') return 'application/x-bibtex';
-  if (ext === '.yml') return 'text/x-yaml';
-  if (ext === '.xml') return 'application/xml';
   return '';
 }
 
@@ -135,6 +140,39 @@ async function copyFilesFromConfig(
       }),
     );
   }
+}
+
+/**
+ * Copy files referenced from image, link, and include nodes, as well as thumbnail
+ */
+async function copyDependentFiles(
+  session: ISession,
+  sourceFile: string,
+  projectPath: string,
+  mecaFolder: string,
+  manifestItems: ManifestItem[],
+) {
+  const cache = castSession(session);
+  if (!cache.$mdast[sourceFile]) {
+    await loadFile(session, sourceFile);
+  }
+  const pre = cache.$mdast[sourceFile]?.pre;
+  if (!pre) return;
+  const { mdast, frontmatter } = pre;
+  const urlNodes = selectAll('image,link', mdast);
+  const fileNodes = selectAll('include', mdast);
+  const filesToCopy = [
+    ...urlNodes.map((node) => (node as any).url),
+    ...fileNodes.map((node) => (node as any).file),
+    frontmatter?.thumbnail,
+  ]
+    .filter((file) => !!file)
+    .map((file) => path.resolve(path.dirname(sourceFile), file))
+    .filter((file) => fs.existsSync(file));
+  filesToCopy.forEach((file) => {
+    const dependency = copyFileMaintainPath(session, file, projectPath, bundleFolder(mecaFolder));
+    addManifestItem(manifestItems, 'article-source', mecaFolder, dependency);
+  });
 }
 
 function writeMecaManifest(manifestItems: ManifestItem[], mecaFolder: string) {
@@ -240,20 +278,16 @@ export async function runMecaExport(
         `Other exports must be built prior to building MECA bundle\nTo resolve this, run: myst build --all`,
       );
     }
-    const jatsDestination = copyFileToFolder(session, jatsOutput, mecaFolder);
-    if (jatsDestination) {
-      addManifestItem(manifestItems, 'article-metadata', mecaFolder, jatsDestination);
+    const jatsDest = copyFileToFolder(session, jatsOutput, mecaFolder);
+    if (jatsDest) {
+      addManifestItem(manifestItems, 'article-metadata', mecaFolder, jatsDest);
     }
     const jatsFiles = path.join(path.dirname(jatsOutput), 'files');
     if (fs.existsSync(jatsFiles)) {
       fs.readdirSync(jatsFiles).forEach((file) => {
         const sourceFile = path.join(jatsFiles, file);
-        const fileDestination = copyFileToFolder(
-          session,
-          sourceFile,
-          path.join(mecaFolder, 'files'),
-        );
-        addManifestItem(manifestItems, 'article-supporting-file', mecaFolder, fileDestination);
+        const fileDest = copyFileToFolder(session, sourceFile, path.join(mecaFolder, 'files'));
+        addManifestItem(manifestItems, 'article-supporting-file', mecaFolder, fileDest);
       });
     }
   });
@@ -272,8 +306,8 @@ export async function runMecaExport(
         `Other exports must be built prior to building MECA bundle\nTo resolve this, run: myst build --all`,
       );
     }
-    const destination = copyFileToFolder(session, manuscriptOutput, mecaFolder);
-    addManifestItem(manifestItems, 'manuscript', mecaFolder, destination);
+    const manuscriptDest = copyFileToFolder(session, manuscriptOutput, mecaFolder);
+    addManifestItem(manifestItems, 'manuscript', mecaFolder, manuscriptDest);
   });
   const project = projectPath
     ? selectors.selectLocalProject(session.store.getState(), projectPath)
@@ -281,12 +315,13 @@ export async function runMecaExport(
   const bundle = bundleFolder(mecaFolder);
   if (projectPath && project) {
     // Copy myst.yml
-    copyFileMaintainPath(
+    const configDest = copyFileMaintainPath(
       session,
       selectors.selectLocalConfigFile(session.store.getState(), projectPath),
       projectPath,
       bundle,
     );
+    addManifestItem(manifestItems, 'article-source', mecaFolder, configDest);
     // Copy requirements and resources
     await copyFilesFromConfig(session, projectPath, mecaFolder, manifestItems);
     // Copy table of contents or write one if it does not exist
@@ -310,11 +345,13 @@ export async function runMecaExport(
         return { page: bib, itemType: 'article-source' };
       }),
     ];
-    projectPages.forEach(({ page, itemType }) => {
-      const pageDest = copyFileMaintainPath(session, page, projectPath, bundle);
-      addManifestItem(manifestItems, itemType, mecaFolder, pageDest);
-    });
-    // TODO: write supporting files images, etc....
+    await Promise.all(
+      projectPages.map(async ({ page, itemType }) => {
+        const pageDest = copyFileMaintainPath(session, page, projectPath, bundle);
+        addManifestItem(manifestItems, itemType, mecaFolder, pageDest);
+        await copyDependentFiles(session, page, projectPath, mecaFolder, manifestItems);
+      }),
+    );
   } else if (article) {
     const articleDest = copyFileToFolder(session, article, bundle);
     addManifestItem(manifestItems, 'article-source', mecaFolder, articleDest);
