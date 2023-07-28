3cf21210760a5966c096aa030d9bc3d7ac2dc543
Franklin Koch <franklinwkoch@gmail.com>
2023-07-28 02:32:49 -0600

    📦 Add node version check to python runner

--- packages/mystmd-py/mystmd_py/main.py
+++ packages/mystmd-py/mystmd_py/main.py
@@ -19,16 +19,30 @@ PATH_TO_BIN_JS = str(
 def main():
     if not NODE_LOCATION:
         sys.exit(
-            'You must install node to run myst\n\n'
+            'You must install node >=16 to run MyST\n\n'
             'We recommend installing the latest LTS release, using your preferred package manager\n'
             'or following instructions here: https://nodejs.org/en/download'
         )
     node = str(pathlib.Path(NODE_LOCATION).resolve())
-    p = subprocess.Popen(
+    version_proc = subprocess.Popen(
+        [node, '-v'], 
+        stdin=subprocess.PIPE, stdout=subprocess.PIPE
+    )
+    version_proc.wait()
+    version = version_proc.communicate()[0].decode()
+    try:
+        ['16', '18', '20'].index(version[1:3])
+    except:
+        sys.exit(
+            f'MyST requires node 16, 18, or 20; you are running node {version[1:3]}.\n\n'
+            'Please update to the latest LTS release, using your preferred package manager\n'
+            'or following instructions here: https://nodejs.org/en/download'
+        )
+    myst_proc = subprocess.Popen(
         [node, PATH_TO_BIN_JS, *sys.argv[1:]],
         stdin=sys.stdin, stdout=sys.stdout
     )
-    sys.exit(p.wait())
+    sys.exit(myst_proc.wait())
 
 
 if __name__ == "__main__":
