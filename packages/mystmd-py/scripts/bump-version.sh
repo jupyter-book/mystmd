d59f1400c72195539980fc3dd3c70c77e05e9fac
Franklin Koch <franklinwkoch@gmail.com>
2023-08-02 11:57:10 -0600

    🔧 Make mystmd-py script cross-platform

--- packages/mystmd-py/scripts/bump-version.sh
+++ packages/mystmd-py/scripts/bump-version.sh
@@ -2,4 +2,8 @@
 CURRENT=`awk -F'"' '/"version": ".+"/{ print $4; exit; }' ../mystmd/package.json`
 PREVIOUS=`awk -F' ' '/version = .+/{ print $3; exit; }' setup.cfg`
 echo "bumping mystmd-py: $PREVIOUS => $CURRENT"
-sed -i "" "s/$PREVIOUS/$CURRENT/" setup.cfg
+if [[ "$OSTYPE" == "darwin"* ]]; then
+    sed -i '' -e "s/$PREVIOUS/$CURRENT/" setup.cfg
+else
+    sed -i'' -e "s/$PREVIOUS/$CURRENT/" setup.cfg
+fi
