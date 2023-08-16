55f0fb3b2efe471f7f22a6096180e0530f269ded
Franklin Koch <franklinwkoch@gmail.com>
2023-08-16 10:18:35 -0600

    🧪 Test multiple outputs are maintained during reduceOutputs

--- packages/myst-cli/src/transforms/outputs.spec.ts
+++ packages/myst-cli/src/transforms/outputs.spec.ts
@@ -106,4 +106,79 @@ describe('reduceOutputs', () => {
     expect(mdast.children.length).toEqual(1);
     expect(mdast.children[0].type).toEqual('image');
   });
+  it('multiple outputs are maintained', async () => {
+    const mdast = {
+      type: 'root',
+      children: [
+        {
+          type: 'output',
+          id: 'abc123',
+          data: [
+            {
+              output_type: 'display_data',
+              execution_count: 3,
+              metadata: {},
+              data: {
+                'image/png': {
+                  content_type: 'image/png',
+                  hash: 'def456',
+                  path: '/my/path/def456.png',
+                },
+                'text/plain': {
+                  content_type: 'text/plain',
+                  hash: 'a6255a8d7ac11cabe5829e143599f112',
+                  path: '/my/path/a6255a8d7ac11cabe5829e143599f112.txt',
+                },
+              },
+            },
+            {
+              output_type: 'display_data',
+              execution_count: 3,
+              metadata: {},
+              data: {
+                'image/png': {
+                  content_type: 'image/png',
+                  hash: 'ghi789',
+                  path: '/my/path/ghi789.png',
+                },
+                'text/plain': {
+                  content_type: 'text/plain',
+                  hash: 'a6255a8d7ac11cabe5829e143599f112',
+                  path: '/my/path/a6255a8d7ac11cabe5829e143599f112.txt',
+                },
+              },
+            },
+          ],
+        },
+        {
+          type: 'output',
+          id: 'jkl012',
+          data: [
+            {
+              output_type: 'display_data',
+              execution_count: 3,
+              metadata: {},
+              data: {
+                'image/png': {
+                  content_type: 'image/png',
+                  hash: 'mno345',
+                  path: '/my/path/mno345.png',
+                },
+                'text/plain': {
+                  content_type: 'text/plain',
+                  hash: 'a6255a8d7ac11cabe5829e143599f112',
+                  path: '/my/path/a6255a8d7ac11cabe5829e143599f112.txt',
+                },
+              },
+            },
+          ],
+        },
+      ],
+    };
+    reduceOutputs(mdast, 'notebook.ipynb', '/my/folder');
+    expect(mdast.children.length).toEqual(3);
+    expect(mdast.children[0].type).toEqual('image');
+    expect(mdast.children[1].type).toEqual('image');
+    expect(mdast.children[2].type).toEqual('image');
+  });
 });
