76dd09449e6c364171359f2ae1f783cb3c4d4416
Andrea <andry.tino@gmail.com>
2023-09-15 08:41:05 +0200

    Add initial changes to build glossaries (not complete as we need to connect the export system to the rest of the application and instruct in the options when the make of glossaries is necessary)

--- packages/jtex/src/export.ts
+++ packages/jtex/src/export.ts
@@ -1,6 +1,7 @@
+import path from 'node:path';
 import type MystTemplate from 'myst-templates';
 
-export function pdfExportCommand(texFile: string, logFile: string, template?: MystTemplate) {
+export function pdfExportCommand(texFile: string, logFile: string, template?: MystTemplate): string {
   const templateYml = template?.getValidatedTemplateYml();
   const engine = templateYml?.build?.engine ?? '-xelatex';
   if (process.platform === 'win32') {
@@ -8,4 +9,9 @@ export function pdfExportCommand(texFile: string, logFile: string, template?: My
   } else {
     return `latexmk -f ${engine} -synctex=1 -interaction=batchmode -file-line-error -latexoption="-shell-escape" ${texFile} &> ${logFile}`;
   }
+}
+
+export function texMakeGlossariesCommand(texFile: string): string {
+  const fileNameNoExt = path.basename(texFile, '.tex');
+  return `makeglossaries ${fileNameNoExt}`;
 }
