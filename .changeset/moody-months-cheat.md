bd09f9178511b4e29864c3c085835d23cbcc10bf
Rowan Cockett <rowanc1@gmail.com>
2023-09-18 13:13:26 -0600

    👯‍♀️ Add directive option `alias`
    You can now use `label` or `name` for all directives

---
+++ .changeset/moody-months-cheat.md
@@ -0,0 +1,7 @@
+---
+'myst-directives': patch
+'myst-parser': patch
+'myst-cli': patch
+---
+
+All instances of `name` options in directives can also use `label`. (e.g. in a figure or equation).
--- packages/myst-parser/tests/directives/directives.spec.ts
+++ packages/myst-parser/tests/directives/directives.spec.ts
@@ -4,11 +4,13 @@ import path from 'node:path';
 import yaml from 'js-yaml';
 import { selectAll } from 'unist-util-select';
 import { mystParse } from '../../src';
+import { VFile } from 'vfile';
 
 type TestCase = {
   title: string;
   markdown: string;
   mdast: Record<string, any>;
+  warnings?: number;
 };
 
 type TestCases = {
@@ -28,13 +30,18 @@ casesList.forEach(({ title, cases }) => {
   describe(title, () => {
     test.each(cases.map((c): [string, TestCase] => [c.title, c]))(
       '%s',
-      (_, { markdown, mdast }) => {
-        const output = mystParse(markdown);
+      (_, { markdown, mdast, warnings = 0 }) => {
+        const vfile = new VFile();
+        const output = mystParse(markdown, { vfile });
         // Dont worry about position
         selectAll('[position]', output).forEach((node) => {
           delete node.position;
         });
         expect(output).toEqual(mdast);
+        if (vfile.messages.length !== warnings) {
+          console.log(vfile.messages);
+        }
+        expect(vfile.messages.length).toBe(warnings);
       },
     );
   });
