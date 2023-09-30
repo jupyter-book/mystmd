41f1845ce082618f68c8e95209a163040afc0db1
Franklin Koch <franklinwkoch@gmail.com>
2023-09-29 18:04:31 -0600

    🐑 Keep track of session clones for error reporting

---
+++ packages/myst-cli/src/session/session.spec.ts
@@ -0,0 +1,78 @@
+import { describe, expect, it } from 'vitest';
+import { addWarningForFile } from 'myst-cli';
+import { Session } from '../session';
+import { RuleId } from 'myst-common';
+
+describe('session warnings', () => {
+  it('getAllWarnings returns for single session', async () => {
+    const session = new Session();
+    addWarningForFile(session, 'my-file', 'my message', 'error', { ruleId: RuleId.bibFileExists });
+    expect(session.getAllWarnings(RuleId.bibFileExists)).toEqual([
+      {
+        file: 'my-file',
+        message: 'my message',
+        kind: 'error',
+        ruleId: RuleId.bibFileExists,
+      },
+    ]);
+  });
+  it('getAllWarnings empty for different rule', async () => {
+    const session = new Session();
+    addWarningForFile(session, 'my-file', 'my message', 'error', {
+      ruleId: RuleId.blockMetadataLoads,
+    });
+    expect(session.getAllWarnings(RuleId.bibFileExists)).toEqual([]);
+  });
+  it('getAllWarnings returns clone warnings', async () => {
+    const session = new Session();
+    const clone = session.clone();
+    addWarningForFile(session, 'my-file-0', 'my message', 'error', {
+      ruleId: RuleId.bibFileExists,
+    });
+    addWarningForFile(clone, 'my-file-1', 'my message', 'error', { ruleId: RuleId.bibFileExists });
+    expect(session.getAllWarnings(RuleId.bibFileExists)).toEqual([
+      {
+        file: 'my-file-0',
+        message: 'my message',
+        kind: 'error',
+        ruleId: RuleId.bibFileExists,
+      },
+      {
+        file: 'my-file-1',
+        message: 'my message',
+        kind: 'error',
+        ruleId: RuleId.bibFileExists,
+      },
+    ]);
+    expect(clone.getAllWarnings(RuleId.bibFileExists)).toEqual([
+      {
+        file: 'my-file-1',
+        message: 'my message',
+        kind: 'error',
+        ruleId: RuleId.bibFileExists,
+      },
+    ]);
+  });
+  it('getAllWarnings deduplicates clone warnings', async () => {
+    const session = new Session();
+    const clone = session.clone();
+    addWarningForFile(session, 'my-file', 'my message', 'error', { ruleId: RuleId.bibFileExists });
+    addWarningForFile(clone, 'my-file', 'my message', 'error', { ruleId: RuleId.bibFileExists });
+    expect(session.getAllWarnings(RuleId.bibFileExists)).toEqual([
+      {
+        file: 'my-file',
+        message: 'my message',
+        kind: 'error',
+        ruleId: RuleId.bibFileExists,
+      },
+    ]);
+    expect(clone.getAllWarnings(RuleId.bibFileExists)).toEqual([
+      {
+        file: 'my-file',
+        message: 'my message',
+        kind: 'error',
+        ruleId: RuleId.bibFileExists,
+      },
+    ]);
+  });
+});
