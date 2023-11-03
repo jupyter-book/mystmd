f7c29db6c6659f66e7b7bf1113eea8cf16256782
Rowan Cockett <rowanc1@gmail.com>
2023-11-03 10:57:27 -0600

    🐛 Citations with trailing punctuation (#720)
    See #719

---
+++ packages/markdown-it-myst/tests/cases.spec.ts
@@ -0,0 +1,45 @@
+import { describe, expect, test, vi } from 'vitest';
+import type Token from 'markdown-it/lib/token';
+import { citationsPlugin } from '../src';
+import fs from 'node:fs';
+import path from 'node:path';
+import yaml from 'js-yaml';
+import MarkdownIt from 'markdown-it';
+
+type TestFile = {
+  title: string;
+  cases: TestCase[];
+};
+type TestCase = {
+  title: string;
+  md: string;
+  tokens: Token[];
+};
+
+const directory = path.join('tests');
+const files = ['citations.yml'];
+
+const only = ''; // Can set this to a test title
+
+const casesList = files
+  .map((file) => ({ name: file, data: fs.readFileSync(path.join(directory, file)).toString() }))
+  .map((file) => {
+    const tests = yaml.load(file.data) as TestFile;
+    tests.title = tests.title ?? file.name;
+    return tests;
+  });
+
+casesList.forEach(({ title, cases }) => {
+  const casesToUse = cases.filter((c) => !only || c.title === only);
+  if (casesToUse.length === 0) return;
+  describe(title, () => {
+    test.each(casesToUse.map((c): [string, TestCase] => [c.title, c]))(
+      '%s',
+      (_, { md, tokens }) => {
+        const mdit = MarkdownIt().use(citationsPlugin);
+        const parsed = mdit.parse(md, {});
+        expect(parsed).containSubset(tokens);
+      },
+    );
+  });
+});
