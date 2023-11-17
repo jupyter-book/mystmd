39d67ec05266349b57cd23423a0eba16f040a9db
Franklin Koch <franklinwkoch@gmail.com>
2023-11-17 11:46:03 -0700

    🔧 Refactor (and test) liftChildren util

---
+++ .changeset/moody-forks-trade.md
@@ -0,0 +1,5 @@
+---
+'myst-common': patch
+---
+
+Refactor (and test) liftChildren util
--- packages/myst-common/src/utils.spec.ts
+++ packages/myst-common/src/utils.spec.ts
@@ -1,8 +1,8 @@
 import { describe, expect, test } from 'vitest';
 import { u } from 'unist-builder';
-import { mergeTextNodes, toText } from './utils';
+import { liftChildren, mergeTextNodes, toText } from './utils';
 
-describe('Test math trasformations', () => {
+describe('Test text utils', () => {
   test('toText', () => {
     const para = u('paragraph', [u('text', { value: 'hello ' }), u('strong', { value: 'there' })]);
     expect(toText(para)).toBe('hello there');
@@ -21,5 +21,48 @@ describe('Test math trasformations', () => {
     expect(x.children?.[0].value).toBe('hihum');
     expect(x.children?.[0].position?.end).toBe('yes'); // Obviously a real position, but you get the idea
     expect(x.children?.[2].value).toBe('xy');
+  });
+});
+
+describe('Test liftChildren', () => {
+  test('liftChildren does not modify tree', () => {
+    const before = u('root', [u('block', [u('paragraph', [u('text', 'value')])])]);
+    const after = u('root', [u('block', [u('paragraph', [u('text', 'value')])])]);
+    liftChildren(before, '_lift');
+    expect(before).toEqual(after);
+  });
+  test('liftChildren lifts one child', () => {
+    const before = u('root', [u('block', [u('paragraph', [u('text', 'value')])])]);
+    const after = u('root', [u('block', [u('text', 'value')])]);
+    liftChildren(before, 'paragraph');
+    expect(before).toEqual(after);
+  });
+  test('liftChildren lifts adjacent node children', () => {
+    const before = u('root', [
+      u('block', [u('paragraph', [u('text', 'one')]), u('paragraph', [u('text', 'two')])]),
+    ]);
+    const after = u('root', [u('block', [u('text', 'one'), u('text', 'two')])]);
+    liftChildren(before, 'paragraph');
+    expect(before).toEqual(after);
+  });
+  test('liftChildren lifts nested node children', () => {
+    const before = u('root', [
+      u('block', [u('paragraph', [u('paragraph', [u('paragraph', [u('text', 'value')])])])]),
+    ]);
+    const after = u('root', [u('block', [u('text', 'value')])]);
+    liftChildren(before, 'paragraph');
+    expect(before).toEqual(after);
+  });
+  test('liftChildren does not lift root', () => {
+    const before = u('root', [u('block', [u('paragraph', [u('text', 'value')])])]);
+    const after = u('root', [u('block', [u('paragraph', [u('text', 'value')])])]);
+    liftChildren(before, 'root');
+    expect(before).toEqual(after);
+  });
+  test('liftChildren does not lift leaf', () => {
+    const before = u('root', [u('block', [u('paragraph', [u('text', 'value')])])]);
+    const after = u('root', [u('block', [u('paragraph', [u('text', 'value')])])]);
+    liftChildren(before, 'text');
+    expect(before).toEqual(after);
   });
 });
