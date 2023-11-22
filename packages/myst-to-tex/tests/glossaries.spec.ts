22ef68b7c140a26ee8a0d5e6f6b41a874ea4ab71
Franklin Koch <franklinwkoch@gmail.com>
2023-11-22 12:36:26 -0700

    📮 Maintain preamble as structured data in myst-to-tex with funcitons to reconstruct later

--- packages/myst-to-tex/tests/glossaries.spec.ts
+++ packages/myst-to-tex/tests/glossaries.spec.ts
@@ -4,7 +4,7 @@ import path from 'node:path';
 import yaml from 'js-yaml';
 import { unified } from 'unified';
 import type { LatexResult } from '../src';
-import mystToTex from '../src';
+import mystToTex, { generatePreamble } from '../src';
 
 type TestCase = {
   title: string;
@@ -31,13 +31,18 @@ const casesList: TestCases[] = fs
 
 casesList.forEach(({ title, latexGlossary, cases }) => {
   describe(title, () => {
-    test.each(cases.map((c): [string, string, TestCase] => [c.title, latexGlossary, c]))('%s', (_, expectedLatexGlossary, { latex, mdast, printGlossaries }) => {
-      const pipe = unified().use(mystToTex, { printGlossaries });
-      pipe.runSync(mdast as any);
-      const file = pipe.stringify(mdast as any);
-      const printedGlossary = printGlossaries ? `\n${expectedLatexGlossary}` : '';
-      const expectedResult = `${latex}${printedGlossary}`;
-      expect((file.result as LatexResult).value).toEqual(expectedResult);
-    });
+    test.each(cases.map((c): [string, string, TestCase] => [c.title, latexGlossary, c]))(
+      '%s',
+      (_, expectedLatexGlossary, { latex, mdast, printGlossaries }) => {
+        const pipe = unified().use(mystToTex, { printGlossaries });
+        pipe.runSync(mdast as any);
+        const file = pipe.stringify(mdast as any);
+        const { suffix } = generatePreamble((file.result as LatexResult).preamble);
+        const latexValue = (file.result as LatexResult).value + suffix;
+        const printedGlossary = printGlossaries ? `\n${expectedLatexGlossary}` : '';
+        const expectedResult = `${latex}${printedGlossary}`;
+        expect(latexValue).toEqual(expectedResult);
+      },
+    );
   });
 });
