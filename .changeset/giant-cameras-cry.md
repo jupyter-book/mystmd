6c9ce6fc878c2c7e2bc26aafc574f743d4453674
Franklin Koch <franklinwkoch@gmail.com>
2023-12-07 01:36:53 -0700

    🐛 Fix error when article is not defined on meca export (#803)

---
+++ .changeset/giant-cameras-cry.md
@@ -0,0 +1,5 @@
+---
+'myst-cli': patch
+---
+
+Fix error when article is not defined on meca export
--- packages/myst-cli/src/build/meca/index.ts
+++ packages/myst-cli/src/build/meca/index.ts
@@ -157,7 +157,7 @@ export async function runMecaExport(
   const toc = tic();
   const { output, articles } = exportOptions;
   // At this point, export options are resolved to contain zero or one articles
-  const article = articles[0];
+  const article = articles?.[0];
   const vfile = new VFile();
   vfile.path = output;
   const fileCopyErrorLogFn = (m: string) => {
