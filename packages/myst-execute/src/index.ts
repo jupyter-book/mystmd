fdbb2ffb20c44f480d3e1e94fb6bdf8c426cea80
Angus Hollands <goosey15@gmail.com>
2024-01-23 18:41:58 +0000

    chore: add which

--- packages/myst-execute/src/index.ts
+++ packages/myst-execute/src/index.ts
@@ -1,4 +1,2 @@
-import type { GenericParent } from 'myst-common';
-
 export { kernelExecutionTransform } from './execute.js';
 export { findExistingJupyterServer, launchJupyterServer } from './manager.js';
