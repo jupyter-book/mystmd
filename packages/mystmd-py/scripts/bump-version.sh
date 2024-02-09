6df63f274205d24c206e5653b86fedf4d025f85a
Angus Hollands <goosey15@gmail.com>
2024-02-09 20:10:25 +0000

    wip: deployment with hatch

--- packages/mystmd-py/scripts/bump-version.sh
+++
@@ -1,9 +0,0 @@
-#!/bin/bash
-CURRENT=`awk -F'"' '/"version": ".+"/{ print $4; exit; }' ../mystmd/package.json`
-PREVIOUS=`awk -F' ' '/version = .+/{ print $3; exit; }' setup.cfg`
-echo "bumping mystmd-py: $PREVIOUS => $CURRENT"
-if [[ "$OSTYPE" == "darwin"* ]]; then
-    sed -i '' -e "s/$PREVIOUS/$CURRENT/" setup.cfg
-else
-    sed -i'' -e "s/$PREVIOUS/$CURRENT/" setup.cfg
-fi
