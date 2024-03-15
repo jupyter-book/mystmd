0767937ba0195d8ad5a4d1b8f9993e68ed9c2089
Angus Hollands <goosey15@gmail.com>
2024-03-15 11:42:18 +0000

    fix: restore content of file

---
+++ packages/myst-roles/src/keystrokes.ts
@@ -0,0 +1,16 @@
+import type { GenericNode, RoleSpec } from 'myst-common';
+
+
+export const keystrokesRole: RoleSpec = {
+  name: 'keystrokes',
+  alias: ['kbd'],
+  body: {
+    type: String,
+    required: true,
+  },
+  run(data) {
+    const body = data.body as string;
+    const link: GenericNode = { type: 'keystrokes',  value: body };
+    return [link];
+  },
+};
