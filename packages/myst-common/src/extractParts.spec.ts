d4f1a186d1649f88899bfaa576329fd266b37c81
Franklin Koch <franklinwkoch@gmail.com>
2024-03-21 12:27:08 -0600

    🛸 Update extractParts to pull implicit parts from sections based on heading

--- packages/myst-common/src/extractParts.spec.ts
+++ packages/myst-common/src/extractParts.spec.ts
@@ -1,6 +1,7 @@
 import { describe, expect, it } from 'vitest';
-import { extractPart } from './extractParts';
+import { extractImplicitPart, extractPart } from './extractParts';
 import type { GenericParent } from '../dist';
+import { copyNode } from './utils';
 
 describe('extractPart', () => {
   it('no part returns undefined', async () => {
@@ -169,6 +170,437 @@ describe('extractPart', () => {
           type: 'block' as any,
           data: { tags: ['other_tag'] },
           children: [{ type: 'text', value: 'also tagged content' }],
+        },
+      ],
+    });
+  });
+});
+
+describe('extractImplicitPart', () => {
+  it('no part returns undefined', async () => {
+    expect(
+      extractImplicitPart({
+        type: 'root',
+        children: [{ type: 'text', value: 'untagged content' }],
+      }),
+    ).toEqual(undefined);
+  });
+  it('part heading/content is removed from tree at top level and returned', async () => {
+    const tree: GenericParent = {
+      type: 'root',
+      children: [
+        {
+          type: 'paragraph',
+          children: [{ type: 'text', value: 'one' }],
+        },
+        {
+          type: 'heading',
+          children: [{ type: 'text', value: 'abstract' }],
+        },
+        {
+          type: 'paragraph',
+          children: [{ type: 'text', value: 'two' }],
+        },
+        {
+          type: 'paragraph',
+          children: [{ type: 'text', value: 'three' }],
+        },
+        {
+          type: 'heading',
+          children: [{ type: 'text', value: 'intro' }],
+        },
+        {
+          type: 'paragraph',
+          children: [{ type: 'text', value: 'four' }],
+        },
+      ],
+    };
+    expect(extractImplicitPart(tree, 'abstract')).toEqual({
+      type: 'root',
+      children: [
+        {
+          type: 'block',
+          data: { part: 'abstract' },
+          children: [
+            {
+              type: 'paragraph',
+              children: [{ type: 'text', value: 'two' }],
+            },
+            {
+              type: 'paragraph',
+              children: [{ type: 'text', value: 'three' }],
+            },
+          ],
+        },
+      ],
+    });
+    expect(tree).toEqual({
+      type: 'root',
+      children: [
+        {
+          type: 'paragraph',
+          children: [{ type: 'text', value: 'one' }],
+        },
+        {
+          type: 'heading',
+          children: [{ type: 'text', value: 'intro' }],
+        },
+        {
+          type: 'paragraph',
+          children: [{ type: 'text', value: 'four' }],
+        },
+      ],
+    });
+  });
+  it('part heading/content is removed from top level block and returned', async () => {
+    const tree: GenericParent = {
+      type: 'root',
+      children: [
+        {
+          type: 'block',
+          children: [
+            {
+              type: 'paragraph',
+              children: [{ type: 'text', value: 'one' }],
+            },
+            {
+              type: 'heading',
+              children: [{ type: 'text', value: 'abstract' }],
+            },
+            {
+              type: 'paragraph',
+              children: [{ type: 'text', value: 'two' }],
+            },
+            {
+              type: 'paragraph',
+              children: [{ type: 'text', value: 'three' }],
+            },
+            {
+              type: 'heading',
+              children: [{ type: 'text', value: 'intro' }],
+            },
+            {
+              type: 'paragraph',
+              children: [{ type: 'text', value: 'four' }],
+            },
+          ],
+        },
+      ],
+    };
+    expect(extractImplicitPart(tree, 'abstract')).toEqual({
+      type: 'root',
+      children: [
+        {
+          type: 'block',
+          data: { part: 'abstract' },
+          children: [
+            {
+              type: 'paragraph',
+              children: [{ type: 'text', value: 'two' }],
+            },
+            {
+              type: 'paragraph',
+              children: [{ type: 'text', value: 'three' }],
+            },
+          ],
+        },
+      ],
+    });
+    expect(tree).toEqual({
+      type: 'root',
+      children: [
+        {
+          type: 'block',
+          children: [
+            {
+              type: 'paragraph',
+              children: [{ type: 'text', value: 'one' }],
+            },
+            {
+              type: 'heading',
+              children: [{ type: 'text', value: 'intro' }],
+            },
+            {
+              type: 'paragraph',
+              children: [{ type: 'text', value: 'four' }],
+            },
+          ],
+        },
+      ],
+    });
+  });
+  it('part heading/content is not removed when nested', async () => {
+    const tree: GenericParent = {
+      type: 'root',
+      children: [
+        {
+          type: 'block',
+          children: [
+            {
+              type: 'block',
+              children: [
+                {
+                  type: 'paragraph',
+                  children: [{ type: 'text', value: 'one' }],
+                },
+                {
+                  type: 'heading',
+                  children: [{ type: 'text', value: 'abstract' }],
+                },
+                {
+                  type: 'paragraph',
+                  children: [{ type: 'text', value: 'two' }],
+                },
+                {
+                  type: 'paragraph',
+                  children: [{ type: 'text', value: 'three' }],
+                },
+                {
+                  type: 'heading',
+                  children: [{ type: 'text', value: 'intro' }],
+                },
+                {
+                  type: 'paragraph',
+                  children: [{ type: 'text', value: 'four' }],
+                },
+              ],
+            },
+          ],
+        },
+      ],
+    };
+    const treeCopy = copyNode(tree);
+    expect(extractImplicitPart(tree, 'abstract')).toEqual(undefined);
+    expect(tree).toEqual(treeCopy);
+  });
+  it('parts from top and block levels concatenate', async () => {
+    const tree: GenericParent = {
+      type: 'root',
+      children: [
+        {
+          type: 'block',
+          children: [
+            {
+              type: 'paragraph',
+              children: [{ type: 'text', value: 'one' }],
+            },
+            {
+              type: 'heading',
+              children: [{ type: 'text', value: 'abstract' }],
+            },
+            {
+              type: 'paragraph',
+              children: [{ type: 'text', value: 'two' }],
+            },
+          ],
+        },
+        {
+          type: 'paragraph',
+          children: [{ type: 'text', value: 'three' }],
+        },
+        {
+          type: 'heading',
+          children: [{ type: 'text', value: 'abstract' }],
+        },
+        {
+          type: 'paragraph',
+          children: [{ type: 'text', value: 'four' }],
+        },
+      ],
+    };
+    expect(extractImplicitPart(tree, 'abstract')).toEqual({
+      type: 'root',
+      children: [
+        {
+          type: 'block',
+          data: { part: 'abstract' },
+          children: [
+            {
+              type: 'paragraph',
+              children: [{ type: 'text', value: 'two' }],
+            },
+          ],
+        },
+        {
+          type: 'block',
+          data: { part: 'abstract' },
+          children: [
+            {
+              type: 'paragraph',
+              children: [{ type: 'text', value: 'four' }],
+            },
+          ],
+        },
+      ],
+    });
+    expect(tree).toEqual({
+      type: 'root',
+      children: [
+        {
+          type: 'block',
+          children: [
+            {
+              type: 'paragraph',
+              children: [{ type: 'text', value: 'one' }],
+            },
+          ],
+        },
+        {
+          type: 'paragraph',
+          children: [{ type: 'text', value: 'three' }],
+        },
+      ],
+    });
+  });
+  it('part headings with no content remain', async () => {
+    const tree: GenericParent = {
+      type: 'root',
+      children: [
+        {
+          type: 'block',
+          children: [
+            {
+              type: 'paragraph',
+              children: [{ type: 'text', value: 'one' }],
+            },
+            {
+              type: 'heading',
+              children: [{ type: 'text', value: 'abstract' }],
+            },
+            {
+              type: 'heading',
+              children: [{ type: 'text', value: 'not abstract' }],
+            },
+            {
+              type: 'paragraph',
+              children: [{ type: 'text', value: 'two' }],
+            },
+          ],
+        },
+        {
+          type: 'paragraph',
+          children: [{ type: 'text', value: 'three' }],
+        },
+        {
+          type: 'paragraph',
+          children: [{ type: 'text', value: 'four' }],
+        },
+        {
+          type: 'heading',
+          children: [{ type: 'text', value: 'abstract' }],
+        },
+      ],
+    };
+    const treeCopy = copyNode(tree);
+    expect(extractImplicitPart(tree, 'abstract')).toEqual(undefined);
+    expect(tree).toEqual(treeCopy);
+  });
+  it('part is not removed from block with part', async () => {
+    const tree: GenericParent = {
+      type: 'root',
+      children: [
+        {
+          type: 'block',
+          data: { part: 'explicit' },
+          children: [
+            {
+              type: 'paragraph',
+              children: [{ type: 'text', value: 'one' }],
+            },
+            {
+              type: 'heading',
+              children: [{ type: 'text', value: 'abstract' }],
+            },
+            {
+              type: 'paragraph',
+              children: [{ type: 'text', value: 'two' }],
+            },
+            {
+              type: 'paragraph',
+              children: [{ type: 'text', value: 'three' }],
+            },
+            {
+              type: 'heading',
+              children: [{ type: 'text', value: 'intro' }],
+            },
+            {
+              type: 'paragraph',
+              children: [{ type: 'text', value: 'four' }],
+            },
+          ],
+        },
+      ],
+    };
+    const treeCopy = copyNode(tree);
+    expect(extractImplicitPart(tree, 'abstract')).toEqual(undefined);
+    expect(tree).toEqual(treeCopy);
+  });
+  it('part heading with format removed', async () => {
+    const tree: GenericParent = {
+      type: 'root',
+      children: [
+        {
+          type: 'paragraph',
+          children: [{ type: 'text', value: 'one' }],
+        },
+        {
+          type: 'heading',
+          children: [
+            { type: 'text', value: 'abs' },
+            { type: 'strong', children: [{ type: 'text', value: 'TRACT' }] },
+          ],
+        },
+        {
+          type: 'paragraph',
+          children: [{ type: 'text', value: 'two' }],
+        },
+        {
+          type: 'paragraph',
+          children: [{ type: 'text', value: 'three' }],
+        },
+        {
+          type: 'heading',
+          children: [{ type: 'text', value: 'intro' }],
+        },
+        {
+          type: 'paragraph',
+          children: [{ type: 'text', value: 'four' }],
+        },
+      ],
+    };
+    expect(extractImplicitPart(tree, 'abstract')).toEqual({
+      type: 'root',
+      children: [
+        {
+          type: 'block',
+          data: { part: 'abstract' },
+          children: [
+            {
+              type: 'paragraph',
+              children: [{ type: 'text', value: 'two' }],
+            },
+            {
+              type: 'paragraph',
+              children: [{ type: 'text', value: 'three' }],
+            },
+          ],
+        },
+      ],
+    });
+    expect(tree).toEqual({
+      type: 'root',
+      children: [
+        {
+          type: 'paragraph',
+          children: [{ type: 'text', value: 'one' }],
+        },
+        {
+          type: 'heading',
+          children: [{ type: 'text', value: 'intro' }],
+        },
+        {
+          type: 'paragraph',
+          children: [{ type: 'text', value: 'four' }],
         },
       ],
     });
--- packages/myst-common/src/extractParts.ts
+++ packages/myst-common/src/extractParts.ts
@@ -1,20 +1,25 @@
 import type { Block } from 'myst-spec-ext';
-import type { GenericParent } from './types.js';
+import type { GenericNode, GenericParent } from './types.js';
 import { remove } from 'unist-util-remove';
 import { selectAll } from 'unist-util-select';
-import { copyNode } from './utils.js';
+import { copyNode, toText } from './utils.js';
+
+function coercePart(part?: string | string[]): string[] {
+  if (!part) {
+    // Prevent an undefined, null or empty part comparison
+    return [];
+  }
+  return typeof part === 'string' ? [part] : part;
+}
 
 /**
  * Selects the block node(s) based on part (string) or tags (string[]).
  * If `part` is a string array, any of the parts will be treated equally.
  */
-export function selectBlockParts(tree: GenericParent, part: string | string[]): Block[] {
-  if (!part) {
-    // Prevent an undefined, null or empty part comparison
-    return [];
-  }
+export function selectBlockParts(tree: GenericParent, part?: string | string[]): Block[] {
+  const parts = coercePart(part);
+  if (parts.length === 0) return [];
   const blockParts = selectAll('block', tree).filter((block) => {
-    const parts = typeof part === 'string' ? [part] : part;
     return parts
       .map((p) => {
         return (
@@ -27,22 +32,105 @@ export function selectBlockParts(tree: GenericParent, part: string | string[]): 
   return blockParts as Block[];
 }
 
+function createPartBlock(
+  children: GenericNode[],
+  part: string,
+  opts?: {
+    removePartData?: boolean;
+  },
+) {
+  const block: GenericParent = { type: 'block', children };
+  if (!opts?.removePartData) {
+    block.data ??= {};
+    block.data.part = part;
+  }
+  return block;
+}
+
+/**
+ * Extract implicit part based on heading name
+ *
+ * Given a tree, search children at the root or block level for a heading
+ * with text matching parts. If such heading is encountered, return a copy of
+ * the subsequent paragraph nodes until a non-paragraph node is encountered.
+ * Heading and paragraph nodes in the original tree are marked for deletion.
+ *
+ * Ignores anything that is already part of a block with explicit part.
+ */
+export function extractImplicitPart(
+  tree: GenericParent,
+  part?: string | string[],
+  opts?: {
+    removePartData?: boolean;
+  },
+): GenericParent | undefined {
+  const parts = coercePart(part);
+  if (parts.length === 0) return;
+  let insideImplicitPart = false;
+  const blockParts: GenericNode[] = [];
+  let paragraphs: GenericNode[] = [];
+  tree.children.forEach((child, index) => {
+    // Add this paragraph to the part
+    if (insideImplicitPart && child.type === 'paragraph') {
+      paragraphs.push(copyNode(child));
+      child.type = '__part_delete__';
+    }
+    // Stop adding things if we didn't just add a paragraph OR we are at the last child
+    if (child.type !== '__part_delete__' || index === tree.children.length - 1) {
+      insideImplicitPart = false;
+      if (paragraphs.length > 0) {
+        blockParts.push(createPartBlock(paragraphs, parts[0], opts));
+        paragraphs = [];
+        selectAll('__part_heading__', tree).forEach((node) => {
+          node.type = '__part_delete__';
+        });
+      }
+    }
+    if (child.type === 'block') {
+      // Do not search blocks already marked explicitly as parts
+      if (child.data?.part) return;
+      // Do not recursively search beyond top-level blocks on root node
+      if (tree.type !== 'root') return;
+      const blockPartsTree = extractImplicitPart(child as GenericParent, parts);
+      if (blockPartsTree) blockParts.push(...blockPartsTree.children);
+    } else if (child.type === 'heading' && parts.includes(toText(child).toLowerCase())) {
+      // Start adding paragraphs to the part after this heading
+      insideImplicitPart = true;
+      child.type = '__part_heading__';
+    }
+  });
+  // Restore part headings if they did not contain any paragraphs
+  selectAll('__part_heading__', tree).forEach((node) => {
+    node.type = 'heading';
+  });
+  if (blockParts.length === 0) return;
+  const partsTree = { type: 'root', children: blockParts } as GenericParent;
+  remove(tree, '__part_delete__');
+  return partsTree;
+}
+
 /**
  * Returns a copy of the block parts and removes them from the tree.
  */
 export function extractPart(
   tree: GenericParent,
-  part: string | string[],
+  part?: string | string[],
   opts?: {
     /** Helpful for when we are doing recursions, we don't want to extract the part again. */
     removePartData?: boolean;
-    /** Ensure that blocks are by default turned to visible */
+    /** Ensure that blocks are by default turned to visible within the part */
     keepVisibility?: boolean;
+    /** Provide an option so implicit section-to-part behavior can be disabled */
+    requireExplicitPart?: boolean;
   },
 ): GenericParent | undefined {
-  const partStrings = typeof part === 'string' ? [part] : part;
+  const partStrings = coercePart(part);
+  if (partStrings.length === 0) return;
   const blockParts = selectBlockParts(tree, part);
-  if (blockParts.length === 0) return undefined;
+  if (blockParts.length === 0) {
+    if (opts?.requireExplicitPart) return;
+    return extractImplicitPart(tree, partStrings);
+  }
   const children = copyNode(blockParts).map((block) => {
     // Ensure the block always has the `part` defined, as it might be in the tags
     block.data ??= {};
