cec2d318be60fc4cc054d23205123c77e4354f69
Franklin Koch <franklinwkoch@gmail.com>
2024-04-10 17:14:12 -0600

    🧪 Run end-to-end tests concurrently but with longer timeout

--- packages/mystmd/tests/endToEnd.spec.ts
+++ packages/mystmd/tests/endToEnd.spec.ts
@@ -26,28 +26,32 @@ function resolve(relative: string) {
   return path.resolve(__dirname, relative);
 }
 
-describe('End-to-end cli export tests', () => {
-  const cases = loadCases('exports.yml');
-  test.each(cases.map((c): [string, TestCase] => [c.title, c]))(
-    '%s',
-    async (_, { cwd, command, outputs }) => {
-      // Clean expected outputs if they already exist
-      await Promise.all(
-        outputs.map(async (output) => {
-          if (fs.existsSync(resolve(output.path))) {
-            await exec(`rm ${resolve(output.path)}`, { cwd: resolve(cwd) });
-          }
-        }),
-      );
-      // Run CLI command
-      await exec(command, { cwd: resolve(cwd) });
-      // Expect correct output
-      outputs.forEach((output) => {
-        expect(fs.existsSync(resolve(output.path))).toBeTruthy();
-        expect(fs.readFileSync(resolve(output.path), { encoding: 'utf-8' })).toEqual(
-          fs.readFileSync(resolve(output.content), { encoding: 'utf-8' }),
+describe.concurrent(
+  'End-to-end cli export tests',
+  () => {
+    const cases = loadCases('exports.yml');
+    test.each(cases.map((c): [string, TestCase] => [c.title, c]))(
+      '%s',
+      async (_, { cwd, command, outputs }) => {
+        // Clean expected outputs if they already exist
+        await Promise.all(
+          outputs.map(async (output) => {
+            if (fs.existsSync(resolve(output.path))) {
+              await exec(`rm ${resolve(output.path)}`, { cwd: resolve(cwd) });
+            }
+          }),
         );
-      });
-    },
-  );
-});
+        // Run CLI command
+        await exec(command, { cwd: resolve(cwd) });
+        // Expect correct output
+        outputs.forEach((output) => {
+          expect(fs.existsSync(resolve(output.path))).toBeTruthy();
+          expect(fs.readFileSync(resolve(output.path), { encoding: 'utf-8' })).toEqual(
+            fs.readFileSync(resolve(output.content), { encoding: 'utf-8' }),
+          );
+        });
+      },
+    );
+  },
+  { timeout: 15000 },
+);
