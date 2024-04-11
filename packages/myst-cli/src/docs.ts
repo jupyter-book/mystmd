5fec6ca34601760c6ac040bca7e183224e6c31df
Markus Ritschel <git@markusritschel.de>
2024-04-11 16:55:20 +0200

    Let installTectonic link point to section in documentation

--- packages/myst-cli/src/docs.ts
+++ packages/myst-cli/src/docs.ts
@@ -1,5 +1,5 @@
 export const docLinks = {
   installNode: 'https://nodejs.org/en/download/',
   installLatex: 'https://mystmd.org/guide/creating-pdf-documents#install-latex',
-  installTectonic: 'https://tectonic-typesetting.github.io/en-US/install.html',
+  installTectonic: 'https://mystmd.org/guide/creating-pdf-documents#install-tectonic'
 };
