3537822cd98fe2664eff8352be24df376df136ca
markusritschel <git@markusritschel.de>
2024-04-11 15:39:00 +0200

    Add tectonic availability check and modify warning if no build tool available

--- packages/myst-cli/src/docs.ts
+++ packages/myst-cli/src/docs.ts
@@ -1,4 +1,5 @@
 export const docLinks = {
   installNode: 'https://nodejs.org/en/download/',
   installLatex: 'https://mystmd.org/guide/creating-pdf-documents#install-latex',
+  installTectonic: 'https://tectonic-typesetting.github.io/en-US/install.html',
 };
