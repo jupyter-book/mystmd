df666c2366b48d824ebb33bc9f561613b4c512ca
Rowan Cockett <rowanc1@gmail.com>
2024-04-19 15:36:55 -0600

    🧱 Add gridItem directive

--- packages/myst-cli/src/process/myst.ts
+++ packages/myst-cli/src/process/myst.ts
@@ -1,6 +1,6 @@
 import { mystParse } from 'myst-parser';
 import { cardDirective } from 'myst-ext-card';
-import { gridDirective } from 'myst-ext-grid';
+import { gridDirectives } from 'myst-ext-grid';
 import { proofDirective } from 'myst-ext-proof';
 import { exerciseDirectives } from 'myst-ext-exercise';
 import { reactiveDirective, reactiveRole } from 'myst-ext-reactive';
@@ -39,7 +39,7 @@ export function parseMyst(
     markdownit: { linkify: true },
     directives: [
       cardDirective,
-      gridDirective,
+      ...gridDirectives,
       reactiveDirective,
       proofDirective,
       ...exerciseDirectives,
--- packages/myst-ext-grid/src/index.ts
+++ packages/myst-ext-grid/src/index.ts
@@ -1,4 +1,19 @@
-import type { DirectiveSpec, DirectiveData, GenericNode } from 'myst-common';
+import {
+  type DirectiveSpec,
+  type DirectiveData,
+  type GenericNode,
+  normalizeLabel,
+} from 'myst-common';
+
+function getColumns(columnString?: string) {
+  const columns = (columnString ?? '1 2 2 3')
+    .split(/\s/)
+    .map((s) => Number(s.trim()))
+    .filter((n) => !Number.isNaN(n))
+    .map((n) => Math.min(Math.max(Math.floor(n), 1), 12)); // Integer between 1 and 12
+  if (columns.length === 0 || columns.length > 4) return [1, 2, 2, 3];
+  return columns;
+}
 
 export const gridDirective: DirectiveSpec = {
   name: 'grid',
@@ -29,12 +44,32 @@ export const gridDirective: DirectiveSpec = {
   },
 };
 
-function getColumns(columnString?: string) {
-  const columns = (columnString ?? '1 2 2 3')
-    .split(/\s/)
-    .map((s) => Number(s.trim()))
-    .filter((n) => !Number.isNaN(n))
-    .map((n) => Math.min(Math.max(Math.floor(n), 1), 12)); // Integer between 1 and 12
-  if (columns.length === 0 || columns.length > 4) return [1, 2, 2, 3];
-  return columns;
-}
+export const gridItemDirective: DirectiveSpec = {
+  name: 'grid-item',
+  options: {
+    label: {
+      type: String,
+      alias: ['name'],
+    },
+    class: {
+      type: String,
+    },
+  },
+  body: {
+    type: 'myst',
+    required: true,
+  },
+  run(data: DirectiveData): GenericNode[] {
+    const { label, identifier } = normalizeLabel(data.options?.label as string | undefined) || {};
+    const div: GenericNode = {
+      type: 'div',
+      children: data.body as unknown as GenericNode[],
+      class: data.options?.class as string | undefined,
+      label,
+      identifier,
+    };
+    return [div];
+  },
+};
+
+export const gridDirectives = [gridDirective, gridItemDirective];
