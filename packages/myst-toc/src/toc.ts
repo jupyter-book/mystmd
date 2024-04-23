76efba2449dda4d84a4635c6271033020024cabc
Angus Hollands <goosey15@gmail.com>
2024-04-23 15:20:54 +0100

    fix: AJV warnings

--- packages/myst-toc/src/toc.ts
+++ packages/myst-toc/src/toc.ts
@@ -2,17 +2,15 @@ import type { TOC } from './types.js';
 import schema from './schema.json';
 import _Ajv from 'ajv';
 
-// Adjust types for ES module
-// @ts-ignore
-const Ajv = _Ajv as unknown as typeof _Ajv.default;
-
 /**
  * Parse a sphinx-external-toc table of contents
  *
  * @param contents: raw TOC yaml
  */
 export function parseTOC(toc: Record<string, unknown>): TOC {
-  const ajv = new Ajv.default();
+  // @ts-ignore
+  const Ajv = _Ajv.default;
+  const ajv = new Ajv();
   const validate = ajv.compile(schema);
   if (!validate(toc)) {
     throw new Error(`The given contents do not form a valid TOC.`);
