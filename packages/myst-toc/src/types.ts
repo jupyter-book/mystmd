a6478a6e69adc6e5d3892a20b8875de7fc1ae1ad
Angus Hollands <goosey15@gmail.com>
2024-04-25 08:22:11 +0100

    feat: add additional attributes

--- packages/myst-toc/src/types.ts
+++ packages/myst-toc/src/types.ts
@@ -1,15 +1,29 @@
+/**
+ * Common attributes for all TOC items
+ * Should be taken as a Partial<>
+ */
+export type CommonEntry = {
+  title: string;
+  hidden: boolean;
+  numbering: string;
+  id: string;
+  part: string;
+};
+
+/**
+ * Entry that groups children, with no associated document
+ */
 export type ParentEntry = {
   children: Entry[];
   title: string;
   class?: string;
-};
+} & Partial<Omit<CommonEntry, 'title'>>;
 
 /**
  * Entry with a path to a single document with or without the file extension
  */
 export type FileEntry = {
   file: string;
-  title?: string;
 } & Partial<ParentEntry>;
 
 /**
@@ -17,12 +31,18 @@ export type FileEntry = {
  */
 export type URLEntry = {
   url: string;
-  title?: string;
 } & Partial<ParentEntry>;
+
+/**
+ * Entry representing several documents through a glob
+ */
+export type PatternEntry = {
+  pattern: string;
+} & Partial<CommonEntry>;
 
 /**
  * Single TOC entry
  */
-export type Entry = FileEntry | URLEntry | ParentEntry;
+export type Entry = FileEntry | URLEntry | PatternEntry | ParentEntry;
 
 export type TOC = Entry[];
