dfb828755c5007c9c602a13cf1c8469b21ba61a9
Angus Hollands <goosey15@gmail.com>
2024-04-25 15:45:42 +0100

    fix: update Python node checks

--- packages/mystmd-py/src/mystmd_py/main.py
+++ packages/mystmd-py/src/mystmd_py/main.py
@@ -14,7 +14,7 @@ def main():
 
     if not NODE_LOCATION:
         raise SystemExit(
-            "You must install node >=16 to run MyST\n\n"
+            "You must install node >=18 to run MyST\n\n"
             "We recommend installing the latest LTS release, using your preferred package manager\n"
             "or following instructions here: https://nodejs.org/en/download"
         )
@@ -27,9 +27,9 @@ def main():
         raise SystemExit(f"MyST could not determine the version of Node.js: {_version}")
 
     major_version = int(major_version_match[1])
-    if not (major_version in {16, 18} or major_version >= 20):
+    if not (major_version in {18, 20} or major_version >= 22):
         raise SystemExit(
-            f"MyST requires node 16, 18, or 20+; you are running node {version[1:3]}.\n\n"
+            f"MyST requires node 18, 20, or 22+; you are running node {version[1:3]}.\n\n"
             "Please update to the latest LTS release, using your preferred package manager\n"
             "or following instructions here: https://nodejs.org/en/download"
         )
