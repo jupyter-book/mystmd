1bda5bff5d7c816338300564c48a88bfb862f838
Angus Hollands <goosey15@gmail.com>
2024-04-29 09:54:41 +0100

    chore: run linter

--- packages/myst-toc/src/guards.ts
+++ packages/myst-toc/src/guards.ts
@@ -1,15 +1,12 @@
 import type { Entry, FileEntry, URLEntry, PatternEntry } from './types.js';
 
-
 export function isFile(entry: Entry): entry is FileEntry {
   return (entry as any).file !== undefined;
 }
 
-
 export function isURL(entry: Entry): entry is URLEntry {
   return (entry as any).url !== undefined;
 }
-
 
 export function isPattern(entry: Entry): entry is PatternEntry {
   return (entry as any).pattern !== undefined;
