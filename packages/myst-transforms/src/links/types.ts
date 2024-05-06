e9b070d7bfd3ea68eedcea821d058de9539a34ae
Franklin Koch <franklinwkoch@gmail.com>
2024-05-06 17:02:21 -0600

    🔧 Myst xref type

--- packages/myst-transforms/src/links/types.ts
+++ packages/myst-transforms/src/links/types.ts
@@ -24,7 +24,7 @@ export type ResolvedExternalReference = ExternalReference & {
 };
 
 export type MystXRef = {
-  identifier: string;
+  identifier?: string;
   html_id?: string;
   kind: string;
   data: string;
