4fef78f0fcf51d8dc76764d930292b11c40e09a2
Rowan Cockett <rowanc1@gmail.com>
2024-05-07 08:35:37 -0600

    Add a test!

---
+++ packages/myst-transforms/src/links/plugin.spec.ts
@@ -0,0 +1,48 @@
+import { describe, expect, test } from 'vitest';
+import { VFile } from 'vfile';
+import type { Link, ResolvedExternalReference } from './types';
+import { MystTransformer } from './myst';
+import { linksTransform } from './plugin';
+
+export const TEST_REFERENCES: ResolvedExternalReference[] = [
+  {
+    key: 'myst-ref',
+    url: 'https://example.com/myst-ref',
+    kind: 'myst',
+    value: {
+      version: '1',
+      myst: '1.2.0',
+      references: [
+        {
+          identifier: 'explicit-figure',
+          kind: 'figure',
+          data: '/my-figure.json',
+          url: '/my-figure',
+        },
+      ],
+    },
+  },
+];
+
+describe('Link Plugin Transformer', () => {
+  test('link text is matched', async () => {
+    const file = new VFile();
+    const t = new MystTransformer(TEST_REFERENCES);
+
+    const link: Link = {
+      type: 'link',
+      url: `xref:myst-ref#explicit-figure`,
+      children: [{ type: 'text', value: 'xref:myst-ref#explicit-figure' }],
+    };
+    linksTransform({ type: 'root', children: [link] }, file, { transformers: [t] });
+    expect(file.messages.length).toEqual(0);
+    expect(link.internal).toBe(false);
+    expect(link.url).toEqual('https://example.com/myst-ref/my-figure');
+    expect(link.dataUrl).toEqual('https://example.com/myst-ref/my-figure.json');
+    expect(link.type).toEqual('crossReference');
+    expect((link as any).remote).toBe(true);
+    expect((link as any).identifier).toEqual('explicit-figure');
+    expect((link as any).label).toEqual('explicit-figure');
+    expect(link.children).toEqual([]);
+  });
+});
