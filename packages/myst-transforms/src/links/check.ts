2cd383d90a18f13454ff2d71458f996101a15dad
Rowan Cockett <rowanc1@gmail.com>
2024-05-06 21:16:53 -0600

    Allow links to have images as children without warning

--- packages/myst-transforms/src/links/check.ts
+++ packages/myst-transforms/src/links/check.ts
@@ -1,12 +1,12 @@
 import { RuleId, fileWarn, toText, type GenericNode, type GenericParent } from 'myst-common';
-import { selectAll } from 'unist-util-select';
+import { select, selectAll } from 'unist-util-select';
 import type { VFile } from 'vfile';
 
 export function checkLinkTextTransform(mdast: GenericParent, vfile: VFile) {
   const linkNodes = selectAll('link,linkBlock,card,crossReference', mdast) as GenericNode[];
   if (linkNodes.length === 0) return;
   linkNodes.forEach((node) => {
-    if (!toText(node.children)) {
+    if (!toText(node.children) && !select('image', node)) {
       fileWarn(vfile, `Link text is empty for <${node.urlSource ?? node.url}>`, {
         node,
         ruleId: RuleId.linkTextExists,
