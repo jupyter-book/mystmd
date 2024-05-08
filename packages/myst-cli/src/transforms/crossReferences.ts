755fb4a09280d2d058b47c564d3b49d13987bb0c
Franklin Koch <franklinwkoch@gmail.com>
2024-05-08 09:22:49 -0600

    🔧 Refactor myst xref loading to separate functions

--- packages/myst-cli/src/transforms/crossReferences.ts
+++ packages/myst-cli/src/transforms/crossReferences.ts
@@ -8,6 +8,45 @@ import type { PageFrontmatter } from 'myst-frontmatter';
 import type { ISession } from '../session/types.js';
 import type { RendererData } from './types.js';
 
+export async function fetchMystXRefData(dataUrl: string, label: string, vfile: VFile) {
+  try {
+    const resp = await fetch(dataUrl);
+    if (resp.ok) {
+      const data: RendererData = await resp.json();
+      return data;
+    }
+  } catch {
+    // data is unset
+  }
+  fileWarn(vfile, `Unable to resolve link text from external MyST reference: ${label}`, {
+    ruleId: RuleId.mystLinkValid,
+    note: 'Could not load data from external project',
+  });
+  return;
+}
+
+export function nodeFromMystXRefData(
+  identifier: string,
+  data: RendererData,
+  label: string,
+  vfile: VFile,
+) {
+  const targets = selectAll(`[identifier=${identifier}]`, data.mdast) as GenericNode[];
+  // Ignore nodes with `identifier` that are not actually the target.
+  // TODO: migrate `identifier` on these node types to `target`
+  const target = targets.find((t) => {
+    return !['crossReference', 'cite', 'footnoteDefinition', 'footnoteReference'].includes(t.type);
+  });
+  if (!target) {
+    fileWarn(vfile, `Unable to resolve link text from external MyST reference: ${label}`, {
+      ruleId: RuleId.mystLinkValid,
+      note: `Could not locate identifier ${identifier} in page content`,
+    });
+    return;
+  }
+  return target;
+}
+
 /**
  * Load external MyST project data to update link text for MyST xrefs
  */
@@ -32,49 +71,15 @@ export async function transformMystXRefs(
   if (nodes.length === 0) return;
   session.log.debug(`Updating link text for ${plural('%s external MyST reference(s)', nodes)}`);
   let number = 0;
-  let data: RendererData | undefined;
   await Promise.all([
     ...nodes.map(async (node: GenericNode) => {
-      try {
-        const resp = await fetch(node.dataUrl);
-        if (resp.ok) {
-          data = await resp.json();
-        }
-      } catch {
-        // data is unset
-      }
-      if (!data) {
-        fileWarn(
-          vfile,
-          `Unable to resolve link text from external MyST reference: ${node.urlSource}`,
-          { ruleId: RuleId.mystLinkValid, note: 'Could not load data from external project' },
-        );
-        return;
-      }
+      const data = await fetchMystXRefData(node.dataUrl, node.urlSource, vfile);
+      if (!data) return;
       if (!node.identifier) {
         // Page references without specific node identifier
         node.children = [{ type: 'text', value: data.frontmatter?.title ?? data.slug ?? '' }];
       } else {
-        const targets = selectAll(`[identifier=${node.identifier}]`, data.mdast) as GenericNode[];
-        // Ignore nodes with `identifier` that are not actually the target.
-        // TODO: migrate `identifier` on these node types to `target`
-        const target = targets.find((t) => {
-          return !['crossReference', 'cite', 'footnoteDefinition', 'footnoteReference'].includes(
-            t.type,
-          );
-        });
-
-        if (!target) {
-          fileWarn(
-            vfile,
-            `Unable to resolve link text from external MyST reference: ${node.urlSource}`,
-            {
-              ruleId: RuleId.mystLinkValid,
-              note: `Could not locate identifier ${node.identifier} in page content`,
-            },
-          );
-          return;
-        }
+        const target = nodeFromMystXRefData(node.identifier, data, node.urlSource, vfile);
         addChildrenFromTargetNode(node as any, target as any, frontmatter.numbering, vfile);
       }
       number += 1;
