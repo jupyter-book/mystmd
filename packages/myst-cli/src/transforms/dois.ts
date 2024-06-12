61b77953f6345751935c9b7c1c389a1a47e8126d
Franklin Koch <franklinwkoch@gmail.com>
2024-06-12 01:12:08 -0600

    🙉 Update doi fetching to resolve 429 too many request errors (#1293)
    * 🙉 Update doi fetching to resolve 429 too many request errors
    
    * 🧹 Linter fixes

--- packages/myst-cli/src/transforms/dois.ts
+++ packages/myst-cli/src/transforms/dois.ts
@@ -1,3 +1,4 @@
+import pLimit from 'p-limit';
 import type { CitationRenderer, CSL } from 'citation-js-utils';
 import { getCitationRenderers, parseBibTeX, parseCSLJSON } from 'citation-js-utils';
 import { doi } from 'doi-utils';
@@ -246,54 +247,70 @@ export async function transformLinkedDOIs(
     citeDois.push(node as Cite);
   });
   if (linkedDois.length === 0 && citeDois.length === 0) return renderer;
-  session.log.debug(
-    `Found ${plural('%s DOI(s)', linkedDois.length + citeDois.length)} to auto link.`,
-  );
+  const total = linkedDois.length + citeDois.length;
+  session.log.debug(`Found ${plural('%s DOI(s)', total)} to auto link.`);
+  const logData = { total, done: false };
+  setTimeout(() => {
+    if (!logData.done) {
+      session.log.info(
+        `⏳ Waiting to resolve up to ${plural('%s DOI(s)', logData.total)} from doi.org...`,
+      );
+    }
+  }, 5000);
   let number = 0;
+  // Currently doi.org is strictly rate limiting their requests
+  const limit = pLimit(3);
   await Promise.all([
-    ...linkedDois.map(async (node) => {
-      const normalized = doi.normalize(node.url)?.toLowerCase();
-      if (!normalized) return false;
-      let cite: SingleCitationRenderer | null = doiRenderer[normalized];
-      if (!cite) {
-        cite = await getCitation(session, vfile, node.url, node);
-        if (cite) number += 1;
-        else return false;
-      }
-      if (cite.remote) {
-        renderer[cite.render.getLabel()] = cite.render;
-      }
-      doiRenderer[normalized] = cite;
-      const citeNode = node as unknown as Cite;
-      citeNode.type = 'cite';
-      citeNode.kind = 'narrative';
-      citeNode.label = cite.render.getLabel();
-      citeNode.identifier = node.url;
-      if (doi.validate(toText(citeNode.children))) {
-        // If the link text is the DOI, update with a citation in a following pass
-        citeNode.children = [];
-      }
-      return true;
-    }),
-    ...citeDois.map(async (node) => {
-      const normalized = doi.normalize(node.label)?.toLowerCase();
-      if (!normalized) return false;
-      let cite: SingleCitationRenderer | null = doiRenderer[normalized];
-      if (!cite) {
-        cite = await getCitation(session, vfile, node.label, node);
-        if (cite) number += 1;
-        else return false;
-      }
-      if (cite.remote) {
-        renderer[cite.render.getLabel()] = cite.render;
-      }
-      doiRenderer[normalized] = cite;
-      node.label = cite.render.getLabel();
-      return true;
-    }),
+    ...linkedDois.map((node) =>
+      limit(async () => {
+        const normalized = doi.normalize(node.url)?.toLowerCase();
+        if (!normalized) return false;
+        let cite: SingleCitationRenderer | null = doiRenderer[normalized];
+        if (!cite) {
+          cite = await getCitation(session, vfile, node.url, node);
+          if (cite) number += 1;
+          else return false;
+        }
+        if (cite.remote) {
+          renderer[cite.render.getLabel()] = cite.render;
+        }
+        doiRenderer[normalized] = cite;
+        const citeNode = node as unknown as Cite;
+        citeNode.type = 'cite';
+        citeNode.kind = 'narrative';
+        citeNode.label = cite.render.getLabel();
+        citeNode.identifier = node.url;
+        if (doi.validate(toText(citeNode.children))) {
+          // If the link text is the DOI, update with a citation in a following pass
+          citeNode.children = [];
+        }
+        return true;
+      }),
+    ),
+    ...citeDois.map((node) =>
+      limit(async () => {
+        const normalized = doi.normalize(node.label)?.toLowerCase();
+        if (!normalized) return false;
+        let cite: SingleCitationRenderer | null = doiRenderer[normalized];
+        if (!cite) {
+          cite = await getCitation(session, vfile, node.label, node);
+          if (cite) number += 1;
+          else return false;
+        }
+        if (cite.remote) {
+          renderer[cite.render.getLabel()] = cite.render;
+        }
+        doiRenderer[normalized] = cite;
+        node.label = cite.render.getLabel();
+        return true;
+      }),
+    ),
   ]);
+  logData.done = true;
   if (number > 0) {
-    session.log.info(toc(`🪄  Linked ${number} DOI${number > 1 ? 's' : ''} in %s for ${path}`));
+    session.log.info(
+      toc(`🪄  Linked ${plural('%s DOI(s)', number)} from doi.org in %s for ${path}`),
+    );
   }
   return renderer;
 }
