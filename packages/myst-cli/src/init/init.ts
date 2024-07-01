613b7aec4e28356a48ba851d238cc3f542f5ad99
Angus Hollands <goosey15@gmail.com>
2024-07-01 18:06:05 +0100

    chore: run prettier

--- packages/myst-cli/src/init/init.ts
+++ packages/myst-cli/src/init/init.ts
@@ -18,11 +18,17 @@ import { fsExists } from '../utils/fsExists.js';
 const VERSION_CONFIG = '# See docs at: https://mystmd.org/guide/frontmatter\nversion: 1\n';
 import { binaryName, homeURL, readableName, baseConfigs } from '../utils/whiteLabelling.js';
 
-const extendsConfigItems = baseConfigs().map(item => `
-    - ${item}`).join("");
-const extendsConfig = extendsConfigItems ? `
+const extendsConfigItems = baseConfigs()
+  .map(
+    (item) => `
+    - ${item}`,
+  )
+  .join('');
+const extendsConfig = extendsConfigItems
+  ? `
   # ${readableName()} base configuration
-  extends:${extendsConfigItems}` : "";
+  extends:${extendsConfigItems}`
+  : '';
 
 function createProjectConfig({ github }: { github?: string } = {}) {
   return `project:${extendsConfig}
