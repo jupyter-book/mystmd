4b1b8d3e37833a776b4ca628439627ea061ed6d3
Franklin Koch <franklinwkoch@gmail.com>
2024-07-30 22:43:18 -0600

    #️⃣ Allow genindex to specify index heading

--- packages/myst-directives/src/indices.ts
+++ packages/myst-directives/src/indices.ts
@@ -59,7 +59,25 @@ export const indexDirective: DirectiveSpec = {
 
 export const genIndexDirective: DirectiveSpec = {
   name: 'genindex',
-  run(): GenericNode[] {
-    return [{ type: 'genindex' }];
+  arg: {
+    type: 'myst',
+    doc: 'Heading to be included in index block',
+  },
+  run(data): GenericNode[] {
+    const children: GenericNode[] = [];
+    if (data.arg) {
+      const parsedArg = data.arg as GenericNode[];
+      if (parsedArg[0]?.type === 'heading') {
+        children.push(...parsedArg);
+      } else {
+        children.push({
+          type: 'heading',
+          depth: 1,
+          children: parsedArg,
+        });
+      }
+    }
+
+    return [{ type: 'genindex', children }];
   },
 };
