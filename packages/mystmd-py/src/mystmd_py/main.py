a75323806a9fec09cf36e38ac33cadd72566dcc2
Angus Hollands <goosey15@gmail.com>
2024-07-31 10:00:21 +0100

    :snake: Don't resolve `node` symlink (#1420)
    * fix: don't resolve `node` symlink & fix version error message
    
    * chore: run black

--- packages/mystmd-py/src/mystmd_py/main.py
+++ packages/mystmd-py/src/mystmd_py/main.py
@@ -18,23 +18,29 @@ def main():
             "We recommend installing the latest LTS release, using your preferred package manager\n"
             "or following instructions here: https://nodejs.org/en/download"
         )
-    node = pathlib.Path(NODE_LOCATION).resolve()
+    node = pathlib.Path(NODE_LOCATION).absolute()
 
-    _version = subprocess.run([node, "-v"], capture_output=True, check=True, text=True).stdout
+    _version = subprocess.run(
+        [node, "-v"], capture_output=True, check=True, text=True
+    ).stdout
     major_version_match = re.match(r"v(\d+).*", _version)
-    
+
     if major_version_match is None:
         raise SystemExit(f"MyST could not determine the version of Node.js: {_version}")
 
     major_version = int(major_version_match[1])
     if not (major_version in {18, 20, 22} or major_version > 22):
         raise SystemExit(
-            f"MyST requires node 18, 20, or 22+; you are running node {version[1:3]}.\n\n"
+            f"MyST requires node 18, 20, or 22+; you are running node {major_version}.\n\n"
             "Please update to the latest LTS release, using your preferred package manager\n"
             "or following instructions here: https://nodejs.org/en/download"
         )
-    os.execve(node, [node.name, PATH_TO_BIN_JS, *sys.argv[1:]], {**os.environ, "MYST_LANG": "PYTHON"})
-  
+    os.execve(
+        node,
+        [node.name, PATH_TO_BIN_JS, *sys.argv[1:]],
+        {**os.environ, "MYST_LANG": "PYTHON"},
+    )
+
 
 if __name__ == "__main__":
     main()
