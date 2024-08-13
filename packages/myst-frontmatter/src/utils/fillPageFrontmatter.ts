621ba5674c2fafebff847c04e70569bcc2cc9281
Franklin Koch <franklinwkoch@gmail.com>
2024-08-12 22:45:25 -0600

    🎓 Add doi to biblio and allow combining frontmatter to fill biblio fields

--- packages/myst-frontmatter/src/utils/fillPageFrontmatter.ts
+++ packages/myst-frontmatter/src/utils/fillPageFrontmatter.ts
@@ -211,6 +211,13 @@ export function fillProjectFrontmatter(
     };
   }
 
+  if (filler.biblio || base.biblio) {
+    frontmatter.biblio = {
+      ...(filler.biblio ?? {}),
+      ...(base.biblio ?? {}),
+    };
+  }
+
   if (!trimUnused) {
     if (filler.bibliography || base.bibliography) {
       frontmatter.bibliography = [
