dca5ae4a44d758c34529fec18124ed287c5f90c9
Angus Hollands <goosey15@gmail.com>
2024-08-22 16:28:56 +0100

    feat: use nodeenv when node.js can't be found

--- packages/mystmd-py/src/mystmd_py/main.py
+++ packages/mystmd-py/src/mystmd_py/main.py
@@ -5,30 +5,76 @@ import shutil
 import subprocess
 import sys
 import re
+import platformdirs
+
+
+NODEENV_VERSION = "18.0.0"
+
+
+def find_installed_node():
+    return shutil.which("node") or shutil.which("node.exe") or shutil.which("node.cmd")
+
+
+def find_nodeenv_path():
+    return platformdirs.user_data_path(
+        appname="myst", appauthor=False, version=NODEENV_VERSION
+    )
+
+
+def create_nodeenv(env_path):
+    command = [
+        sys.executable,
+        "-m",
+        "nodeenv",
+        "-v",
+        f"--node={NODEENV_VERSION}",
+        "--prebuilt",
+        "--clean-src",
+        env_path,
+    ]
+    subprocess.run(command, check=True)
+
+
+def find_any_node(binary_path):
+    node_path = find_installed_node()
+    if node_path is not None:
+        return pathlib.Path(node_path).absolute(), binary_path
+
+    nodeenv_path = find_nodeenv_path()
+    if not nodeenv_path.exists():
+        print(f"🔍 Couldn't find installed `node`.\n\n⚙️ Installing Node.js in {nodeenv_path}")
+        create_nodeenv(nodeenv_path)
+
+    new_path = os.pathsep.join(
+        [*binary_path.split(os.pathsep), str(nodeenv_path / "bin")]
+    )
+    return nodeenv_path / "bin" / "node", new_path
 
 
 def main():
-    NODE_LOCATION = (
-        shutil.which("node") or shutil.which("node.exe") or shutil.which("node.cmd")
-    )
-    PATH_TO_BIN_JS = (pathlib.Path(__file__).parent / "myst.cjs").resolve()
+    # Find NodeJS (and potential new PATH)
+    binary_path = os.environ.get("PATH", os.defpath)
+    try:
+        node_path, os_path = find_any_node(binary_path)
+    except Exception as err:
+        import traceback
 
-    if not NODE_LOCATION:
+        traceback.print_exc()
         raise SystemExit(
             "You must install node >=18 to run MyST\n\n"
+            "As NodeJS could not be found, an attempt to install a local node environment was made, but failed\n\n"
             "We recommend installing the latest LTS release, using your preferred package manager\n"
             "or following instructions here: https://nodejs.org/en/download"
-        )
-    node = pathlib.Path(NODE_LOCATION).absolute()
+        ) from err
 
+    # Check version
     _version = subprocess.run(
-        [node, "-v"], capture_output=True, check=True, text=True
+        [node_path, "-v"], capture_output=True, check=True, text=True
     ).stdout
     major_version_match = re.match(r"v(\d+).*", _version)
 
     if major_version_match is None:
         raise SystemExit(f"MyST could not determine the version of Node.js: {_version}")
-
     major_version = int(major_version_match[1])
     if not (major_version in {18, 20, 22} or major_version > 22):
         raise SystemExit(
@@ -36,19 +82,24 @@ def main():
             "Please update to the latest LTS release, using your preferred package manager\n"
             "or following instructions here: https://nodejs.org/en/download"
         )
+   
+    # Find path to compiled JS 
+    js_path = (pathlib.Path(__file__).parent / "myst.cjs").resolve()
 
-    node_args = [PATH_TO_BIN_JS, *sys.argv[1:]]
-    node_env = {**os.environ, "MYST_LANG": "PYTHON"}
+    # Build args for Node.js process
+    node_args = [js_path, *sys.argv[1:]]
+    node_env = {**os.environ, "MYST_LANG": "PYTHON", "PATH": os_path}
+
+    # Invoke appropriate binary for platform
     if platform.system() == "Windows":
-        result = subprocess.run([node, *node_args], env=node_env)
+        result = subprocess.run([node_path, *node_args], env=node_env)
         sys.exit(result.returncode)
     else:
         os.execve(
-            node,
-            [node.name, *node_args],
+            node_path,
+            [node_path.name, *node_args],
             node_env,
         )
-
 
 if __name__ == "__main__":
     main()
