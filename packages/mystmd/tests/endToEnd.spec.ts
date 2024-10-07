febae86903a444334c914d323efd6844b2ec5447
Rowan Cockett <rowanc1@gmail.com>
2024-10-07 10:19:25 -0600

    tests

--- packages/mystmd/tests/endToEnd.spec.ts
+++ packages/mystmd/tests/endToEnd.spec.ts
@@ -26,40 +26,41 @@ function resolve(relative: string) {
   return path.resolve(__dirname, relative);
 }
 
+const only = '';
+
 describe.concurrent(
   'End-to-end cli export tests',
   () => {
     const cases = loadCases('exports.yml');
-    test.each(cases.map((c): [string, TestCase] => [c.title, c]))(
-      '%s',
-      async (_, { cwd, command, outputs }) => {
-        // Clean expected outputs if they already exist
-        await Promise.all(
-          outputs.map(async (output) => {
-            if (fs.existsSync(resolve(output.path))) {
-              await exec(`rm ${resolve(output.path)}`, { cwd: resolve(cwd) });
-            }
-          }),
-        );
-        // Run CLI command
-        await exec(command, { cwd: resolve(cwd) });
-        // Expect correct output
-        outputs.forEach((output) => {
-          expect(fs.existsSync(resolve(output.path))).toBeTruthy();
-          if (path.extname(output.content) === '.json') {
-            expect(
-              JSON.parse(fs.readFileSync(resolve(output.path), { encoding: 'utf-8' })),
-            ).toMatchObject(
-              JSON.parse(fs.readFileSync(resolve(output.content), { encoding: 'utf-8' })),
-            );
-          } else {
-            expect(fs.readFileSync(resolve(output.path), { encoding: 'utf-8' })).toEqual(
-              fs.readFileSync(resolve(output.content), { encoding: 'utf-8' }),
-            );
+    test.each(
+      cases.filter((c) => !only || c.title === only).map((c): [string, TestCase] => [c.title, c]),
+    )('%s', async (_, { cwd, command, outputs }) => {
+      // Clean expected outputs if they already exist
+      await Promise.all(
+        outputs.map(async (output) => {
+          if (fs.existsSync(resolve(output.path))) {
+            await exec(`rm ${resolve(output.path)}`, { cwd: resolve(cwd) });
           }
-        });
-      },
-    );
+        }),
+      );
+      // Run CLI command
+      await exec(command, { cwd: resolve(cwd) });
+      // Expect correct output
+      outputs.forEach((output) => {
+        expect(fs.existsSync(resolve(output.path))).toBeTruthy();
+        if (path.extname(output.content) === '.json') {
+          expect(
+            JSON.parse(fs.readFileSync(resolve(output.path), { encoding: 'utf-8' })),
+          ).toMatchObject(
+            JSON.parse(fs.readFileSync(resolve(output.content), { encoding: 'utf-8' })),
+          );
+        } else {
+          expect(fs.readFileSync(resolve(output.path), { encoding: 'utf-8' })).toEqual(
+            fs.readFileSync(resolve(output.content), { encoding: 'utf-8' }),
+          );
+        }
+      });
+    });
   },
   { timeout: 15000 },
 );
