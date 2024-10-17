7c964249e1b92cdff839e3fa21c2abb6edc4b81c
Rowan Cockett <rowanc1@gmail.com>
2024-10-17 12:00:45 -0600

    🐛 Keep alive for myst init commands

--- packages/mystmd/src/init.ts
+++ packages/mystmd/src/init.ts
@@ -17,14 +17,14 @@ export function makeInitCLI(program: Command) {
     .addOption(makeWriteTOCOption())
     .addOption(makeGithubPagesOption())
     .addOption(makeGithubCurvenoteOption())
-    .action(clirun(Session, init, program));
+    .action(clirun(Session, init, program, { keepAlive: true }));
   return command;
 }
 
 // The default command runs `myst init` with no arguments
 export function addDefaultCommand(program: Command) {
   program.action(async (...args: any[]) => {
-    if (program.args.length === 0) return clirun(Session, init, program)(args);
+    if (program.args.length === 0) return clirun(Session, init, program, { keepAlive: true })(args);
     console.error(
       `${chalk.red(`Invalid command: `)}${chalk.bold(program.args.join(' '))}\n\n${chalk.dim(
         'See --help for a list of available commands.\n',
