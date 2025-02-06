40097614e7d67ceebf3849fd66f36806343eae9d
Angus Hollands <goosey15@gmail.com>
2025-02-06 16:33:49 +0000

    feat: add `skip_execution` frontmatter field
    This is page-only, and probably only temporary; in future we'll want
    to fold this in as part of our execution strategies refactor

--- packages/myst-frontmatter/src/page/validators.ts
+++ packages/myst-frontmatter/src/page/validators.ts
@@ -51,6 +51,12 @@ export function validatePageFrontmatterKeys(value: Record<string, any>, opts: Va
   if (defined(value.jupytext)) {
     output.jupytext = validateJupytext(value.jupytext, incrementOptions('jupytext', opts));
   }
+  if (defined(value.skip_execution)) {
+    output.skip_execution = validateBoolean(
+      value.skip_execution,
+      incrementOptions('skip_execution', opts),
+    );
+  }
   if (defined(value.enumerator)) {
     output.enumerator = validateString(value.enumerator, incrementOptions('enumerator', opts));
   }
