a226703b2c05ad517bb60f6f190fe19fa224b93a
Angus Hollands <goosey15@gmail.com>
2025-02-07 19:35:33 -0700

    chore: remove utils

--- packages/myst-compat/src/utils.ts
+++
@@ -1,7 +0,0 @@
-export function squeeze<T>(record: Record<string, T | unknown>) {
-  Object.keys(record).forEach((key) => {
-    if (record[key] === undefined) {
-      delete record[key];
-    }
-  });
-}
