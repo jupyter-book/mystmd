1a0c56447d37255d352a3447de86d9c9535cbfc0
Franklin Koch <franklinwkoch@gmail.com>
2025-02-27 19:13:29 -0700

    🔧 Support page and section tocs

--- packages/myst-directives/src/toc.ts
+++ packages/myst-directives/src/toc.ts
@@ -1,17 +1,44 @@
-import type { DirectiveSpec, DirectiveData, GenericNode } from 'myst-common';
+import type { VFile } from 'vfile';
+import { type DirectiveSpec, type DirectiveData, type GenericNode, fileError } from 'myst-common';
 import { addCommonDirectiveOptions, commonDirectiveOptions } from './utils.js';
+
+const CONTEXTS = ['project', 'page', 'section'];
 
 export const tocDirective: DirectiveSpec = {
   name: 'toc',
-  alias: ['tableofcontents', 'table-of-contents', 'toctree'],
+  alias: ['tableofcontents', 'table-of-contents', 'toctree', 'contents'],
   arg: {
     type: 'myst',
     doc: 'Heading to be included with table of contents',
   },
   options: {
+    context: {
+      type: String,
+      doc: 'Table of Contents context; one of project, page, or section',
+      alias: ['kind'],
+    },
+    depth: {
+      type: Number,
+      doc: 'Number of levels to include in Table of Contents; by default, all levels will be included',
+      alias: ['maxdepth'],
+    },
     ...commonDirectiveOptions('toc'),
   },
-  run(data: DirectiveData): GenericNode[] {
+  run(data: DirectiveData, vfile: VFile): GenericNode[] {
+    let context = data.options?.context
+      ? (data.options.context as string)
+      : data.name === 'contents'
+        ? 'section'
+        : 'project';
+    if (!CONTEXTS.includes(context)) {
+      fileError(vfile, `Unknown context for ${data.name} directive: ${context}`);
+      context = 'project';
+    }
+    let depth = data.options?.depth as number | undefined;
+    if (depth != null && depth < 1) {
+      fileError(vfile, `Table of Contents 'depth' must be a number greater than 0`);
+      depth = undefined;
+    }
     const children: GenericNode[] = [];
     if (data.arg) {
       const parsedArg = data.arg as GenericNode[];
@@ -20,12 +47,13 @@ export const tocDirective: DirectiveSpec = {
       } else {
         children.push({
           type: 'heading',
-          depth: 1,
+          depth: 2,
+          enumerated: false,
           children: parsedArg,
         });
       }
     }
-    const toc = { type: 'toc', children };
+    const toc = { type: 'toc', kind: context, depth, children };
     addCommonDirectiveOptions(data, toc);
     return [toc];
   },
--- packages/myst-transforms/src/toc.ts
+++ packages/myst-transforms/src/toc.ts
@@ -1,6 +1,6 @@
-import { fileWarn, type GenericNode, type GenericParent } from 'myst-common';
+import { fileError, fileWarn, toText, type GenericNode, type GenericParent } from 'myst-common';
 import type { List, Text } from 'myst-spec';
-import type { Link, ListItem } from 'myst-spec-ext';
+import type { Heading, Link, ListItem } from 'myst-spec-ext';
 import { selectAll } from 'unist-util-select';
 import type { VFile } from 'vfile';
 
@@ -33,14 +33,15 @@ function listItemFromPages(pages: ProjectPage[], projectSlug?: string) {
     type: 'text',
     value: `${enumerator ? `${enumerator} ` : ''}${title}`,
   };
-  const child: Text | Link = slug
-    ? ({
-        type: 'link',
-        url: `${projectSlug ? `/${projectSlug}` : ''}/${slug}`,
-        internal: true,
-        children: [text],
-      } as Link)
-    : text;
+  const child: Text | Link =
+    slug != null
+      ? ({
+          type: 'link',
+          url: `${projectSlug ? `/${projectSlug}` : ''}/${slug}`,
+          internal: true,
+          children: [text],
+        } as Link)
+      : text;
   const item: ListItem = {
     type: 'listItem',
     children: [child],
@@ -51,22 +52,126 @@ function listItemFromPages(pages: ProjectPage[], projectSlug?: string) {
   return item;
 }
 
+function listFromHeadings(headings: Heading[]): List {
+  if (headings.length === 0) return { type: 'list', children: [] };
+  let ignore = false;
+  const depth = headings[0].depth;
+  const children = headings
+    .map((heading, index) => {
+      if (ignore) return undefined;
+      if (heading.depth < depth) ignore = true;
+      if (heading.depth !== depth) return undefined;
+      return listItemFromHeadings(headings.slice(index));
+    })
+    .filter((item): item is ListItem => !!item);
+  return { type: 'list', children };
+}
+
+function listItemFromHeadings(headings: Heading[]) {
+  if (headings.length === 0) return;
+  const { children, enumerator, depth, identifier } = headings[0];
+  const text: Text = {
+    type: 'text',
+    value: `${enumerator ? `${enumerator} ` : ''}${toText(children)}`,
+  };
+  const child: Text | Link = identifier
+    ? ({
+        type: 'link',
+        url: `#${identifier}`,
+        internal: true,
+        children: [text],
+        suppressImplicitWarning: true,
+      } as Link)
+    : text;
+  const item: ListItem = {
+    type: 'listItem',
+    children: [child],
+  };
+  if (headings[1] && headings[1].depth > depth) {
+    item.children.push(listFromHeadings(headings.slice(1)));
+  }
+  return item;
+}
+
 export function buildTocTransform(
   mdast: GenericParent,
   vfile: VFile,
-  pages: ProjectPage[],
+  pages?: ProjectPage[],
   projectSlug?: string,
 ) {
-  if (pages.length === 0) return;
-  const tocs = selectAll('toc', mdast) as GenericNode[];
-  if (!tocs.length) return;
-  if (pages[0].level !== 1) {
-    fileWarn(vfile, `First page of Table of Contents must be level 1`);
+  const tocHeadings = selectAll('toc > heading', mdast);
+  const tocsAndHeadings = selectAll('toc,heading', mdast).filter((item) => {
+    // Do not include toc headings anywhere in this transform
+    return !tocHeadings.includes(item);
+  }) as GenericNode[];
+  if (!tocsAndHeadings.find((node) => node.type === 'toc')) return;
+  const projectTocs = tocsAndHeadings.filter(
+    (node) => node.type === 'toc' && node.kind === 'project',
+  );
+  const pageTocs = tocsAndHeadings.filter((node) => node.type === 'toc' && node.kind === 'page');
+  const sectionTocs = tocsAndHeadings.filter(
+    (node) => node.type === 'toc' && node.kind === 'section',
+  );
+  if (projectTocs.length) {
+    if (!pages) {
+      fileError(vfile, `Pages not available to build Table of Contents`);
+    } else {
+      if (pages[0].level !== 1) {
+        fileWarn(vfile, `First page of Table of Contents must be level 1`);
+      }
+      projectTocs.forEach((toc) => {
+        const filteredPages = toc.depth ? pages.filter((page) => page.level <= toc.depth) : pages;
+        toc.type = 'block';
+        delete toc.kind;
+        toc.data = { part: 'toc:project' };
+        if (!toc.children) toc.children = [];
+        toc.children.push(listFromPages(filteredPages, projectSlug));
+      });
+    }
   }
-  tocs.forEach((toc) => {
-    toc.type = 'block';
-    toc.data = { part: 'toc' };
-    if (!toc.children) toc.children = [];
-    toc.children.push(listFromPages(pages, projectSlug));
-  });
+  if (pageTocs.length) {
+    const headings = tocsAndHeadings.filter((node) => node.type === 'heading') as Heading[];
+    if (headings.length === 0) {
+      fileWarn(vfile, `No page headings found for Table of Contents`);
+    } else {
+      if (Math.min(...headings.map((h) => h.depth)) !== headings[0].depth) {
+        fileWarn(vfile, 'Page heading levels do not start with highest level');
+      }
+      pageTocs.forEach((toc) => {
+        const filteredHeadings = toc.depth
+          ? headings.filter((heading) => heading.depth - headings[0].depth < toc.depth)
+          : headings;
+        toc.type = 'block';
+        delete toc.kind;
+        toc.data = { part: 'toc:page' };
+        if (!toc.children) toc.children = [];
+        toc.children.push(listFromHeadings(filteredHeadings));
+      });
+    }
+  }
+  if (sectionTocs.length) {
+    tocsAndHeadings.forEach((toc, index) => {
+      if (toc.type !== 'toc' || toc.kind !== 'section') return;
+      const headings = tocsAndHeadings
+        .slice(index + 1)
+        .filter((h) => h.type === 'heading') as Heading[];
+      if (headings.length === 0) {
+        fileWarn(vfile, `No section headings found for Table of Contents`);
+      } else {
+        const filteredHeadings = toc.depth
+          ? headings.filter((heading) => heading.depth - headings[0].depth < toc.depth)
+          : headings;
+        toc.type = 'block';
+        delete toc.kind;
+        toc.data = { part: 'toc:section' };
+        if (!toc.children) toc.children = [];
+        const nextSection = filteredHeadings.findIndex((h) => h.depth < filteredHeadings[0].depth);
+        toc.children.push(
+          listFromHeadings(
+            nextSection === -1 ? filteredHeadings : filteredHeadings.slice(0, nextSection),
+          ),
+        );
+      }
+    });
+  }
 }
