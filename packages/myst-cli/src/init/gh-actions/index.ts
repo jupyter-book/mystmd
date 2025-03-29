ad949aaf5e62fe2d1971e9903b1d8ffb11cf6e1c
Chris Holdgraf <choldgraf@gmail.com>
2025-03-29 13:29:14 -0700

    Update packages/myst-cli/src/init/gh-actions/index.ts

--- packages/myst-cli/src/init/gh-actions/index.ts
+++ packages/myst-cli/src/init/gh-actions/index.ts
@@ -28,10 +28,6 @@ env:
   # `BASE_URL` determines, relative to the root of the domain, the URL that your site is served from.
   # E.g., if your site lives at `https://mydomain.org/myproject`, set `BASE_URL=/myproject`.
   # If, instead, your site lives at the root of the domain, at `https://mydomain.org`, set `BASE_URL=''".
-  #
-  # You may need to change this to \`${
-    isGithubIO ? 'BASE_URL: /${{ github.event.repository.name }}' : "BASE_URL: ''"
-  }\`
   ${
     isGithubIO
       ? `BASE_URL: '' # Not required for '${username}.github.io' domain. Other repos will need to set this!`
