7e2a114e6c4ccec85a86a3310295da40ab40f018
Angus Hollands <goosey15@gmail.com>
2025-04-08 17:38:41 +0100

    🪄 Improve error handling during cross reference resolution (#1942)
    * fix: improve error handling of cross references
    
    * chore: add changeset

--- packages/myst-cli/src/transforms/crossReferences.ts
+++ packages/myst-cli/src/transforms/crossReferences.ts
@@ -39,36 +39,39 @@ async function fetchMystData(
   urlSource: string | undefined,
   vfile: VFile,
 ) {
-  let note: string;
-  if (dataUrl) {
-    const filename = mystDataFilename(dataUrl);
-    const cacheData = loadFromCache(session, filename, { maxAge: XREF_MAX_AGE });
-    if (cacheData) {
-      return JSON.parse(cacheData) as MystData;
-    }
-    try {
-      const resp = await session.fetch(dataUrl);
-      if (resp.ok) {
-        const data = (await resp.json()) as MystData;
-        writeToCache(session, filename, JSON.stringify(data));
-        return data;
-      }
-    } catch {
-      // data is unset
-    }
-    note = 'Could not load data from external project';
-  } else {
-    note = 'Data source URL unavailable';
+  const onError = (note: string): undefined => {
+    fileWarn(
+      vfile,
+      `Unable to resolve link text from external MyST reference: ${urlSource ?? dataUrl ?? ''}`,
+      {
+        ruleId: RuleId.mystLinkValid,
+        note,
+      },
+    );
+  };
+  if (!dataUrl) {
+    return onError('Data source URL unavailable');
   }
-  fileWarn(
-    vfile,
-    `Unable to resolve link text from external MyST reference: ${urlSource ?? dataUrl ?? ''}`,
-    {
-      ruleId: RuleId.mystLinkValid,
-      note,
-    },
-  );
-  return;
+
+  const filename = mystDataFilename(dataUrl);
+  const cacheData = loadFromCache(session, filename, { maxAge: XREF_MAX_AGE });
+  if (cacheData) {
+    return JSON.parse(cacheData) as MystData;
+  }
+  let data: MystData;
+  try {
+    const resp = await session.fetch(dataUrl);
+    if (!resp.ok) {
+      return onError('Could not fetch data from URL');
+    }
+    data = (await resp.json()) as MystData;
+  } catch {
+    return onError('Could not load fetched data');
+    // data is unset
+  }
+
+  writeToCache(session, filename, JSON.stringify(data));
+  return data;
 }
 
 export async function fetchMystLinkData(session: ISession, node: Link, vfile: VFile) {
