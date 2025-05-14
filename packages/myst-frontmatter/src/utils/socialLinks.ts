23ab1c96f79a4107b0e24a57a7df8a4a5c605b15
Kira Evans <contact@kne42.me>
2025-05-14 12:47:17 -0700

    fix: resolve some logic errors in social link validation

--- packages/myst-frontmatter/src/utils/socialLinks.ts
+++ packages/myst-frontmatter/src/utils/socialLinks.ts
@@ -37,32 +37,36 @@ export const SOCIAL_LINKS_ALIASES = {
 export function validateSocialLinks(
   input: any,
   opts: ValidationOptions,
-  output: SocialLinks = {},
+  output?: SocialLinks,
 ): SocialLinks | undefined {
   const value = output
     ? input
     : validateObjectKeys(input, { optional: SOCIAL_LINKS_KEYS, alias: SOCIAL_LINKS_ALIASES }, opts);
 
+  if (value === undefined) return undefined;
+
+  const result = output ?? {};
+
   if (defined(value.url)) {
-    output.url = validateUrl(value.url, incrementOptions('url', opts));
+    result.url = validateUrl(value.url, incrementOptions('url', opts));
   }
   if (defined(value.github)) {
-    output.github = validateString(value.github, incrementOptions('github', opts));
+    result.github = validateString(value.github, incrementOptions('github', opts));
   }
   if (defined(value.bluesky)) {
-    output.bluesky = validateString(value.bluesky, incrementOptions('bluesky', opts));
+    result.bluesky = validateString(value.bluesky, incrementOptions('bluesky', opts));
   }
   if (defined(value.mastodon)) {
-    output.mastodon = validateString(value.mastodon, incrementOptions('mastodon', opts));
+    result.mastodon = validateString(value.mastodon, incrementOptions('mastodon', opts));
   }
   if (defined(value.linkedin)) {
-    output.linkedin = validateUrl(value.linkedin, incrementOptions('linkedin', opts));
+    result.linkedin = validateUrl(value.linkedin, incrementOptions('linkedin', opts));
   }
   if (defined(value.threads)) {
-    output.threads = validateString(value.threads, incrementOptions('threads', opts));
+    result.threads = validateString(value.threads, incrementOptions('threads', opts));
   }
   if (defined(value.twitter)) {
-    output.twitter = validateString(value.twitter, incrementOptions('twitter', opts));
+    result.twitter = validateString(value.twitter, incrementOptions('twitter', opts));
   }
-  return output;
+  return result;
 }
