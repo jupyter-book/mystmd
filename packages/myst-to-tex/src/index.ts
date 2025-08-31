c09695f4eaa42474a1eeb9c1a935b46d097645c2
Joseph C Wang <joequant@gmail.com>
2025-09-01 05:42:44 +0800

    add toc node

--- packages/myst-to-tex/src/index.ts
+++ packages/myst-to-tex/src/index.ts
@@ -472,6 +472,19 @@ const handlers: Record<string, Handler> = {
       state.renderChildren(node);
     }
   },
+  toc(node, state) {
+    const title = node.children?.[0];
+    if (title) {
+       state.write('\\renewcommand{\\contentsname}{');
+       state.text(toText(title));
+       state.write('}\n');
+    }
+    if (node.depth) {
+       state.write(`\\setcounter{tocdepth}{${node.depth}}\n`);
+    }
+    state.write('\\tableofcontents\n');
+  },
+
 };
 
 class TexSerializer implements ITexSerializer {
