ceb0467e8ccbacdca4130f923826e651229dd033
choldgraf <choldgraf@gmail.com>
2025-11-11 16:27:17 -0800

    Add support for notebook cell output scroll metadata

--- packages/myst-spec-ext/src/types.ts
+++ packages/myst-spec-ext/src/types.ts
@@ -257,6 +257,7 @@ export type Output = Node &
     id?: string;
     data?: any[]; // MinifiedOutput[]
     visibility?: Visibility;
+    scroll?: boolean;
     children?: (FlowContent | ListContent | PhrasingContent)[];
   };
 
