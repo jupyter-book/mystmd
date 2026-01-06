e7b8fc3cc9fe07e8f23959c8094d093471398948
Thierry Parmentelat <thierry.parmentelat@inria.fr>
2026-01-06 11:52:44 +0100

    Configure API URL (#2554)
    * can define API_URL environment variable to get templates info at some other API server
    
    * feat: add pluggable API URL
    
    * message logged with debug vs log
    
    Co-authored-by: Angus Hollands <goosey15@gmail.com>
    
    ---------
    
    Co-authored-by: Angus Hollands <goosey15@gmail.com>

--- packages/myst-templates/src/session.ts
+++ packages/myst-templates/src/session.ts
@@ -4,10 +4,16 @@ import type { ISession } from './types.js';
 import { default as nodeFetch } from 'node-fetch';
 import type { RequestInfo, RequestInit, Response } from 'node-fetch';
 
+const DEFAULT_API_URL = 'https://api.mystmd.org';
+
 export class Session implements ISession {
-  API_URL = 'https://api.mystmd.org';
+  API_URL: string;
   log: Logger;
   constructor(opts?: { logger?: Logger }) {
+    // use env variable if set
+    this.API_URL = process.env.API_URL ?? DEFAULT_API_URL;
+    // trailing slashes will cause issues
+    this.API_URL = this.API_URL.replace(/\/+$/, '');
     this.log = opts?.logger ?? chalkLogger(LogLevel.debug);
   }
 
