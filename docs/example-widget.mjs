c69e4552b1d51e13e83379a2f24863cade98ee47
Angus Hollands <goosey15@gmail.com>
2026-02-20 12:35:01 +0000

    docs: improve text in widget

--- docs/example-widget.mjs
+++ docs/example-widget.mjs
@@ -1,15 +1,25 @@
 function render({ model, el }) {
-  let count = () => model.get('count');
+  // Setup quick-access to state
+  const getCount = () => model.get('count');
+  const setCount = (count) => model.set('count', count);
+
+  // Create button
   let btn = document.createElement('button');
   btn.classList.add('counter-button');
-  btn.innerHTML = `count is ${count()}`;
+  btn.innerHTML = `count is ${getCount()}`;
+
+  // Handle button click
   btn.addEventListener('click', () => {
-    model.set('count', count() + 1);
+    setCount(getCount() + 1);
     model.save_changes();
   });
+  // Update text when count changes
   model.on('change:count', () => {
-    btn.innerHTML = `count is ${count()}`;
+    btn.innerHTML = `count is ${getCount()}`;
   });
   el.appendChild(btn);
+
+  // Destructor to clean-up when MyST is finished with us!
+  return () => btn.remove();
 }
 export default { render };
