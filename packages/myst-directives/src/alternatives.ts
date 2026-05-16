a66b82aca62e92260d0d96dfa174df8e7e775410
Angus Hollands <goosey15@gmail.com>
2026-05-16 12:53:06 +0100

    fix: just select what we want
    It's hard to detect errors vs wierd body structure

--- packages/myst-directives/src/alternatives.ts
+++ packages/myst-directives/src/alternatives.ts
@@ -1,7 +1,7 @@
 import type { Alternatives } from 'myst-spec-ext';
 import { addCommonDirectiveOptions, commonDirectiveOptions } from './utils.js';
-import { type DirectiveSpec, type DirectiveData, type GenericNode, fileError } from 'myst-common';
-import type { VFile } from 'vfile';
+import { type DirectiveSpec, type DirectiveData, type GenericNode } from 'myst-common';
+import { selectAll } from 'unist-util-select';
 
 export const alternativesDirective: DirectiveSpec = {
   name: 'alternatives',
@@ -11,26 +11,16 @@ export const alternativesDirective: DirectiveSpec = {
     type: 'myst',
     doc: 'The body of the alternative. If there is no title and the body starts with bold text or a heading, that content will be used as the alternative title.',
   },
-  run(data: DirectiveData, vfile: VFile): GenericNode[] {
-    let rawChildren: GenericNode[] = (data.body as GenericNode[]) ?? [];
-
-    // Multiple images are presented as paragraph of images.
-    // We could also walk down the tree, but this is only temporary and a 
-    // reasonable assumption
-    if (rawChildren.length === 1 && rawChildren[0].type === 'paragraph') {
-      rawChildren = rawChildren[0].children as any[];
-    }
-    const children = rawChildren.filter((n) => n.type === 'image' || n.type === 'anywidget');
-
-    if (children.length !== rawChildren.length) {
-      console.dir(children);
-      console.dir(rawChildren);
-      fileError(vfile, `alternatives can only contain images or anywidgets`);
-    }
+  run(data: DirectiveData): GenericNode[] {
+    const root = {
+      type: 'root',
+      children: (data.body as GenericNode[]) ?? [],
+    };
+    const children = selectAll('image,anywidget', root);
 
     const alternative: Alternatives = {
       type: 'alternatives',
-      children: children as any[],
+      children: children,
     };
     addCommonDirectiveOptions(data, alternative);
     return [alternative];
