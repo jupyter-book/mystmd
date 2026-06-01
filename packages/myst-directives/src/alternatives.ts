f9271d5787e015c2f163e8e5d58af810bae96c4b
Angus Hollands <goosey15@gmail.com>
2026-06-01 22:25:03 +0100

    refactor: don't validate

--- packages/myst-directives/src/alternatives.ts
+++ packages/myst-directives/src/alternatives.ts
@@ -1,26 +1,21 @@
 import type { Alternatives } from 'myst-spec-ext';
 import { addCommonDirectiveOptions, commonDirectiveOptions } from './utils.js';
 import { type DirectiveSpec, type DirectiveData, type GenericNode } from 'myst-common';
-import { selectAll } from 'unist-util-select';
+
+const nodeType = 'alternatives';
 
 export const alternativesDirective: DirectiveSpec = {
-  name: 'alternatives',
+  name: nodeType,
   doc: 'Alternatives group together a selection of similar representations of some content. For example, a video and still image for static exports.',
-  options: commonDirectiveOptions('alternative'),
+  options: commonDirectiveOptions(nodeType),
   body: {
     type: 'myst',
     doc: 'The body of the alternative. If there is no title and the body starts with bold text or a heading, that content will be used as the alternative title.',
   },
   run(data: DirectiveData): GenericNode[] {
-    const root = {
-      type: 'root',
-      children: (data.body as GenericNode[]) ?? [],
-    };
-    const children = selectAll('image,anywidget', root);
-
     const alternative: Alternatives = {
-      type: 'alternatives',
-      children: children,
+      type: nodeType,
+      children: (data.body as GenericNode[]) ?? [],
     };
     addCommonDirectiveOptions(data, alternative);
     return [alternative];
